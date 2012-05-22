10e6b9f6890b42a60493877cc4a164eecf64b432
Michiel Dethmers <git@phplist.com>
2012-05-22 14:44:05 +0000

    clean up

--- public_html/lists/admin/class.phplistmailer.php
+++ public_html/lists/admin/class.phplistmailer.php
@@ -575,7 +575,6 @@ class PHPlistMailer extends PHPMailer {
 
       ## we don't really use multiple to's so pass that on to phpmailer, if there are any
       if (!$this->SingleTo || !USE_LOCAL_SPOOL) {
-#output('Not single or not spool');
         return parent::MailSend($header,$body);
       }
       if (!is_dir(USE_LOCAL_SPOOL) || !is_writable(USE_LOCAL_SPOOL)) {
@@ -589,7 +588,6 @@ class PHPlistMailer extends PHPMailer {
         $header .= 'To: '.$GLOBALS['developer_email'].$this->LE;
       }
       $header .= "Subject: ".$this->EncodeHeader($this->Subject).$this->LE;
-#output('Writing spool');
       $fname = tempnam(USE_LOCAL_SPOOL,'msg');
       file_put_contents($fname,$header."\n".$body);
       file_put_contents($fname.'.S',$this->Sender);
