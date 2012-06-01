ad32c1af2f55e17b074aef7df64759602063f6c6
Michiel Dethmers <git@phplist.com>
2012-06-01 18:27:49 +0000

    generate test file for admin imports

--- scripts/mkimp.pl
+++ scripts/mkimp.pl
@@ -1,19 +1,32 @@
 #!/usr/bin/perl
-# generate a test file to import
-open F,">testimport.txt";
+# generate a test files to import
+
+## CSV import
+open F,">testimport-csv.txt";
 print F "email\tName\tAddr1\tAddr2\tTown\tPostcode\tforeign key\tsend this user HTML emails\n";
 $start = 20;
 $num = 10000;
 for ($i=$start;$i<$start+$num;$i++) {
-  print F "$i\@reallynonexistentdomain.com\tTestUser $i\tAddr line 1\tAddr line2\tTown\tPostcode $i\tABC $i\t1\n";
+  print F "$i\@mailinator.com\tTestUser $i\tAddr line 1\tAddr line2\tTown\tPostcode $i\tABC $i\t1\n";
 }
 close(F);
 
-open F,">testimport2.txt";
+# Plain list of emails import
+open F,">testimport-plain.txt";
 $start = 0;
 for ($i=$start;$i<$start+$num;$i++) {
-  print F "michiel-$i\@phplist.com\n";
+  print F "importtest-$i\@mailinator.com\n";
 }
 close(F);
 
+open F,">testimport-admin.txt";
+print F "email\tloginname\tpassword\n";
+$start = 20;
+$num = 10000;
+for ($i=$start;$i<$start+$num;$i++) {
+  $pass = `pwgen -1`;
+  chomp($pass);
+  print F "$i\@reallynonexistentdomain.com\tTest Admin $i\t$pass\n";
+}
+close(F);
 
