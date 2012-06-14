d4660579d157dd2804ae452edd9e5574a49c2275
Michiel Dethmers <git@phplist.com>
2012-06-14 14:02:39 +0000

    update short2long

--- scripts/short2long.sh
+++ scripts/short2long.sh
@@ -2,25 +2,35 @@
 # script to replace short php tags <? with long one <?php
 
 echo "replacing <? with <?php"
-for i in `fgrep -rilI -e "<? " --exclude-dir .svn public_html*`; do
+for i in `fgrep -rilI -e "<?" --exclude-dir .svn public_html*|grep -v short2long`; do
    echo $i;
-   sed -i "s/<? /<?php /" $i;
+   sed -i "s/<?/<?php/" $i;
 done
-# remove accidentally doubled ones
+# revert doubled ones
 echo "replacing <?phpphp with <?php"
-for i in `fgrep -rilI -e "<?phpphp" --exclude-dir .svn public_html*`; do
+for i in `fgrep -rilI -e "<?phpphp" --exclude-dir .svn public_html*|grep -v short2long`; do
    echo $i;
    sed -i "s/<?phpphp/<?php/" $i;
 done
+echo "replacing <?php php with <?php"
+for i in `fgrep -rilI -e "<?php php" --exclude-dir .svn public_html*|grep -v short2long`; do
+   echo $i;
+   sed -i "s/<?php php/<?php/" $i;
+done
 echo "replacing <?= with <?php echo "
-for i in `fgrep -rilI -e "<?=" --exclude-dir .svn public_html*`; do
+for i in `fgrep -rilI -e "<?=" --exclude-dir .svn public_html*|grep -v short2long`; do
    echo $i;
    sed -i "s/<?=/<?php echo /" $i;
 done
 echo "replacing <?php= with <?php echo "
-for i in `fgrep -rilI -e "<?php=" --exclude-dir .svn public_html*`; do
+for i in `fgrep -rilI -e "<?php=" --exclude-dir .svn public_html*|grep -v short2long`; do
    echo $i;
    sed -i "s/<?php=/<?php echo /" $i;
+done
+echo "replacing <?phpxml with <?xml "
+for i in `fgrep -rilI -e "<?phpxml" --exclude-dir .svn public_html*|grep -v short2long`; do
+   echo $i;
+   sed -i "s/<?phpxml/<?xml/" $i;
 done
 
 
