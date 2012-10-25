a22fa6affd9af53b714380bfefbdbf0ad56930f1
Michiel Dethmers <git@phplist.com>
2012-10-25 15:35:19 +0000

    add flag "editorprovider" to allow a plugin to implement the html editor

--- public_html/lists/admin/defaultplugin.php
+++ public_html/lists/admin/defaultplugin.php
@@ -35,6 +35,15 @@ class phplistPlugin {
   
   public $needI18N = 0;
   
+  ## set to true, if this plugin provides the WYSIWYG editor for the send page
+  /* the plugin will then need to implement:
+   * 
+   * function editor($fieldname,$fieldvalue)
+   * 
+   * which returns the HTML for the editor.
+   */
+  public $editorProvider = false;
+  
   /* array of pages in this plugin to add to the menu
    * 
    * example format: 
