92fd9cf5f7c09ceaba1b6c260ee316f70ddbf248
Michiel Dethmers <git@phplist.com>
2012-11-13 14:16:26 +0000

    #16694 from patch - make some strings translatable

--- public_html/lists/admin/catlists.php
+++ public_html/lists/admin/catlists.php
@@ -26,8 +26,8 @@ $subselect .= '(category is null or category = "")';
 $categories = listCategories();
 
 if (!sizeof($categories)) {
-  print '<p>'.$I18N->get('No list categories have been defined').'</p>';
-  print '<p>'.$I18N->get('Once you have set up a few categories, come back to this page to classify your lists with your categories.').'</p>';
+  print '<p>'.s('No list categories have been defined').'</p>';
+  print '<p>'.s('Once you have set up a few categories, come back to this page to classify your lists with your categories.').'</p>';
   print '<p>'.PageLinkButton('configure&id=list_categories',$I18N->get('Configure Categories')).'</p>';
   print '<br/>';
   return;
@@ -43,26 +43,26 @@ if (!empty($_POST['category']) && is_array($_POST['category'])) {
 $req = Sql_Query(sprintf('select * from %s %s',$tables['list'],$subselect));
 
 if (!Sql_Affected_Rows()) {
-  print Info($GLOBALS['I18N']->get('All lists have already been assigned a category'),true);
+  print Info(s('All lists have already been assigned a category'),true);
 }
 
-$ls = new WebblerListing($I18N->get('Categorise lists'));
+$ls = new WebblerListing(s('Categorise lists'));
 $aListCategories = listCategories();
 if (sizeof($aListCategories)) {
   while ($row = Sql_Fetch_Assoc($req)) {
     $ls->addELement($row['id']);
     $ls->addColumn($row['id'],$GLOBALS['I18N']->get('Name'),$row['name']);
     $catselect = '<select name="category['.$row['id'].']">';
-    $catselect .= '<option value="">-- '.$GLOBALS['I18N']->get('choose category').'</option>';
+    $catselect .= '<option value="">-- '.s('choose category').'</option>';
     foreach ($aListCategories as $category) {
       $category = trim($category);
       $catselect .= sprintf('<option value="%s" %s>%s</option>',$category,$category == $row['category'] ? 'selected="selected"':'',$category);
     }
     $catselect .= '</select>';
-    $ls->addColumn($row['id'],$GLOBALS['I18N']->get('Category'),$catselect);
+    $ls->addColumn($row['id'],s('Category'),$catselect);
   }  
 }
-$ls->addButton('save','javascript:document.categoryedit.submit();');
+$ls->addButton(s('save'),'javascript:document.categoryedit.submit();');
 
 print $ls->display();
 print '</form>';
