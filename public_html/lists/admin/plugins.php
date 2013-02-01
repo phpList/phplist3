1a226e51c802bdc9070f0e91154a568aabe701ec
Michiel Dethmers <git@phplist.com>
2013-02-01 00:24:24 +0000

    plugin page update to allow install from remote url

--- public_html/lists/admin/plugins.php
+++ public_html/lists/admin/plugins.php
@@ -1,43 +1,115 @@
+<?php
+require_once dirname(__FILE__).'/accesscheck.php';
+#$_POST['pluginurl'] = '';
 
-<h3>Extend phpList by creating your own plugins</h3>
+if (!empty($_POST['pluginurl'])) {
+  //if (!verifyToken()) {
+    //print Error(s('Invalid security token, please reload the page and try again'));
+    //return;
+  //}
+  
+  $packageurl = $_POST['pluginurl'];
+  ## verify the url against known locations, and require it to be "zip".
+  //if (!preg_match('/^https?:\/\/github\.com\/.*\.zip$/i',$packageurl)) {
+    //print Error(s('Invalid download URL, please reload the page and try again'));
+    //return;
+  //}
+  print '<h3>'.s('Fetching plugin').'</h3>';
+  
+  $packagefile = file_get_contents($packageurl);
+  $filename = basename($packageurl);
 
-<p>Plugins are a powerful method to add functionality to phpList, without changing the core phpList code.
-That way your changes will continue to work, when you need to upgrade to a newer version of phpList.</p>
-
-<p>Particularly when you want to integrate phpList into your own systems, plugins are the way to go.</p>
-
-<h4>How to start writing your own plugins.</h4>
-<i>Knowledge of PHP required.</i>
-<p>Plugins can influence the functionality in a variety of places. If necessary, you can create pages that will show up in the backend interface, but in many cases, this may not be necessary.</p>
-<ul>
-<li>Create a class file "myplugin.php" and store it in the plugins folder in the phpList "admin" folder.
-<p><b>Note:</b> the ClassName needs to be the same as the file it is stored in, with the php extension. Eg "myplugin" is stored as "myplugin.php".</p>
-<pre class="examplecode">
-&lt;?php
-class myplugin extends phplistPlugin {
-  public $name = "This is my plugin";     ## name of the plugin
-  public $coderoot = "myplugin";          ## location of files for the plugin, relative to the plugin root 
-  public $enabled = 1;                    ## enable this plugin
-  public $version = "0.1";                ## version of this plugin
-  public $authors = "Your Name";          ## who made it
-
-  function myplugin() {
+  file_put_contents($GLOBALS['tmpdir'].'/phpListPlugin-'.$filename,$packagefile);
+  print '<h3>'.s('Installing plugin').'</h3>';
+  $zip = new ZipArchive;
+  if ($zip->open($GLOBALS['tmpdir'].'/phpListPlugin-'.$filename)) {
+  
+  /* the zip may have a variety of directory structures, as Github seems to add at least one for the "branch" of 
+   * the project and then the developer has some more. 
+   * We look for a directory called "plugins" and place it's contents in the plugins folder.
+   */
+  
+ 
+#  var_dump($zip);
+  //echo "numFiles: " . $zip->numFiles . "\n";
+  //echo "status: " . $zip->status  . "\n";
+  //echo "statusSys: " . $zip->statusSys . "\n";
+  //echo "filename: " . $zip->filename . "\n";
+  //echo "comment: " . $zip->comment . "\n";  
+  
+  $extractList = array();
+  $dir_prefix = '';
+  for ($i=0; $i<$zip->numFiles;$i++) {
+#      echo "index: $i<br/>\n";
+#    var_dump($zip->statIndex($i));
+    $zipItem = $zip->statIndex($i);
+    if (preg_match('~^([^/]+)/plugins/~',$zipItem['name'],$regs)) {
+      array_push($extractList,$zipItem['name']);
+      $dir_prefix = $regs[1];
+    }
+  }
+  var_dump($extractList);
+  var_dump($dir_prefix);
+  @mkdir($GLOBALS['tmpdir'].'/phpListPluginInstall',0755);
+  $destination = PLUGIN_ROOTDIR;
+#  $destination = $GLOBALS['tmpdir'].'/phpListPluginDestination';
+  @mkdir($destination,0755);
+  if (is_writable($destination)) {
+    if ($zip->extractTo($GLOBALS['tmpdir'].'/phpListPluginInstall',$extractList)) {
+      $extractedDir = opendir($GLOBALS['tmpdir'].'/phpListPluginInstall/'.$dir_prefix.'/plugins/');
+      while ($dirEntry = readdir($extractedDir)) {
+        if (!preg_match('/^\./',$dirEntry)) {
+          print $dirEntry .'<br/>';
+          @rename($GLOBALS['tmpdir'].'/phpListPluginInstall/'.$dir_prefix.'/plugins/'.$dirEntry,
+            $destination.'/'.$dirEntry);
+        }  
+      }
+      ## clean up
+      delFsTree($GLOBALS['tmpdir'].'/phpListPluginInstall');
+      
+      print s('Plugin installed successfully');
+      $zip->close();   
+      print '<hr/>'.PageLinkButton('plugins',s('Continue'));
+      return;
+    }
+  } else {
+    Error(s('Plugin directory is not writable'));
+  }
+  } else {
+    Error(s('Invalid plugin package'));
   }
 
-  function adminmenu() {
-    return array(
-      "myplugin" => "View my plugin"
-    );
-  }
-
+  print s('Plugin installation failed');
+  $zip->close();   
+  print '<hr/>'.PageLinkButton('plugins',s('Continue'));
+  return;
 }
-?&gt;
-</pre>
-</li>
-<li>Find the file "defaultplugin.php" in the "admin" folder and you will find the methods you can implement in your plugin. The methods are commented, so you have an idea of which one is triggered where.
-</li>
-<li>Sign up to the <a href="http://www.phplist.com/developers">Developers Mailinglist</a> and start discussing your plugin and how to achieve it.
-</li>
-</ul>
 
+if (!class_exists('ZipArchive')) {
+  Warn(s('PHP has no <a href="http://php.net/zip">Zip capability</a>, cannot continue'));
+  return;
+}
+
+if (!is_writable(PLUGIN_ROOTDIR)) {
+  Warn(s('The plugin root directory is not writable, please install plugins manually'));
+} else {
+  print '<h3>'.s('Install a new plugin').'</h3>';
+  print formStart();
+  print '<fieldset>
+      <label for="pluginurl">'.s('Plugin package URL').'</label>
+      <div type="field"><input type="text" id="pluginurl" name="pluginurl" /></div>
+      <button type="submit" name="download">'.s('Install plugin').'</button>
+      </fieldset>';
+}
+
+$ls = new WebblerListing(s('Installed plugins'));
+
+foreach ($GLOBALS['plugins'] as $pluginname => $plugin) {
+  $ls->addElement($pluginname);
+  $ls->addColumn($pluginname,s('name'),$plugin->name);
+  $ls->addColumn($pluginname,s('enabled'),$plugin->enabled ? $GLOBALS['img_tick'] : $GLOBALS['img_cross']);
+  
+}
+
+print $ls->display();
 
