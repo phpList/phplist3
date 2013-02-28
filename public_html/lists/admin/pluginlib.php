6477518156c0c52c376c377dea5ab38e8219bad3
Michiel Dethmers <git@phplist.com>
2013-02-28 21:35:33 +0000

    add "PLUGIN_ROOTDIRS" as a config, to allow easier development of plugins. The "PLUGIN_ROOTDIR" remains the place to install them, but the other dirs can be used to point to the developer's git repo

--- public_html/lists/admin/pluginlib.php
+++ public_html/lists/admin/pluginlib.php
@@ -6,25 +6,55 @@ $GLOBALS['editorplugin'] = false;
 if (!defined("PLUGIN_ROOTDIR")) {
   define("PLUGIN_ROOTDIR","notdefined");
 }
-if (is_dir(PLUGIN_ROOTDIR)) {
-  include_once dirname(__FILE__). "/defaultplugin.php";
-  $files = array();
-  $dh=opendir(PLUGIN_ROOTDIR);
-  while (false!==($file = readdir($dh))) {
-    if ($file != "." && $file != ".." &&
-      !preg_match("/~$/",$file) &&
-      is_file(PLUGIN_ROOTDIR."/".$file) &&
-      preg_match("/\.php$/",$file) ){
-      array_push($files,$file);
+
+$pluginRootDirs = array();
+if (defined('PLUGIN_ROOTDIRS')) {
+  $pluginRootDirs = explode(';',PLUGIN_ROOTDIRS);
+} else {
+  $pluginRootDirs = array(PLUGIN_ROOTDIR);
+}  
+
+include_once dirname(__FILE__). "/defaultplugin.php";
+$pluginFiles = array();
+
+foreach ($pluginRootDirs as $pluginRootDir) {
+#  print '<h3>'.$pluginRootDir.'</h3>';
+  if (is_dir($pluginRootDir)) {
+    $dh = opendir($pluginRootDir);
+    while (false!==($file = readdir($dh))) {
+      if ($file != "." && $file != ".." && !preg_match("/~$/",$file)) {
+#        print $pluginRootDir.' '.$file.'<br/>';
+        if (is_file($pluginRootDir."/".$file) && preg_match("/\.php$/",$file)) {
+#          print "ADD $file<br/>";
+          array_push($pluginFiles,$pluginRootDir."/".$file);
+        } elseif (is_dir($pluginRootDir.'/'.$file.'/plugins')) {
+#         print 'SUBROOT'.$pluginRootDir.' '.$file.'<br/>';
+          $subRoot = $pluginRootDir.'/'.$file.'/plugins';
+          $subDir = opendir($subRoot);
+          while (false!==($subFile = readdir($subDir))) {
+            if (is_file($subRoot.'/'.$subFile) && preg_match("/\.php$/",$subFile) ) {
+#              print "ADD $subFile<br/>";
+              array_push($pluginFiles,$subRoot.'/'.$subFile);
+            } else {
+#              print "NOT A FILE: $subRoot.'/'.$subFile<br/>";
+            }
+          }
+        } else {
+#          print 'NOT A DIR: '.$pluginRootDir.'/'.$file.'/plugins<br/>';
+        }
+      }
     }
+    closedir($dh);
   }
-  closedir($dh);
-  asort($files);
-  reset($files);
-  foreach ($files as $file) {
-    list($className,$ext) = explode(".",$file);
-    if (preg_match("/[\w]+/",$className) && !in_array($className,$GLOBALS['plugins_disabled'])) {
-      include_once PLUGIN_ROOTDIR."/" . $file;
+}
+#var_dump($pluginFiles);exit;
+
+
+foreach ($pluginFiles as $file) {
+  list($className,$ext) = explode(".",basename($file));
+  if (preg_match("/[\w]+/",$className) && !in_array($className,$GLOBALS['plugins_disabled'])) {
+    if (!class_exists($className)) {
+      include_once $file;
       if (class_exists($className)) {
         eval("\$pluginInstance = new ". $className ."();");
         if ($pluginInstance->enabled) {
@@ -43,17 +73,17 @@ if (is_dir(PLUGIN_ROOTDIR)) {
       #print "$className = ".$pluginInstance->name."<br/>";
     }
   }
-  $GLOBALS['pluginsendformats'] = array();
-  foreach ($GLOBALS['plugins'] as $className => $pluginInstance) {
-    $plugin_sendformats = $pluginInstance->sendFormats();
-    if (is_array($plugin_sendformats) && sizeof($plugin_sendformats)) {
-      foreach ($plugin_sendformats as $val => $desc) {
-        $val = preg_replace("/\W/",'',strtolower(trim($val)));
-        $GLOBALS['pluginsendformats'][$val] = $className;
-      }
+}
+$GLOBALS['pluginsendformats'] = array();
+foreach ($GLOBALS['plugins'] as $className => $pluginInstance) {
+  $plugin_sendformats = $pluginInstance->sendFormats();
+  if (is_array($plugin_sendformats) && sizeof($plugin_sendformats)) {
+    foreach ($plugin_sendformats as $val => $desc) {
+      $val = preg_replace("/\W/",'',strtolower(trim($val)));
+      $GLOBALS['pluginsendformats'][$val] = $className;
     }
   }
-} 
+}
 
 /*
   * central function to call a method on all plugins
