42898a9176862559d5efe1acfa938f214cacd4a8
Michiel Dethmers <git@phplist.com>
2013-04-10 12:42:18 +0000

    redo help page

--- public_html/lists/admin/community.php
+++ public_html/lists/admin/community.php
@@ -1,8 +1,29 @@
 
-<!-- every thing is in the info file -->
 
 <style>
     #globalhelp{display:none;}
 </style>
 
-<?php include('info/'.$_SESSION['adminlanguage']['iso'].'/community.php'); ?>
+<div class="tabbed">
+  <ul>
+    <li><a href="#gethelp"><?php print s('How to get help')?></a></li>
+    <li><a href="#helpout"><?php print s('How to help out')?></a></li>
+  </ul>
+  
+<div id="gethelp">
+<h3><?php print s('Get help with phpList')?></h3>
+<p><?php print s('To be written, in the meantime ')?></p>
+<p><a href="http://www.phplist.com/support"><?php print s('visit support section on the phpList website')?></a>.</p>
+</div> 
+
+<div id="helpout">  
+<h3><?php print s('Help out with phpList')?></h3>
+<p><?php print s('To be written, in the meantime ')?></p>
+<p><a href="http://www.phplist.com/developers"><?php print s('visit developers section on the phpList website')?></a>.</p>
+</div>
+
+</div>
+
+<?php
+# include('info/'.$_SESSION['adminlanguage']['iso'].'/community.php'); 
+?>
