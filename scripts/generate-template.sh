efee1f726d322cf8400dd258e87190a06df05864
Michiel Dethmers <git@phplist.com>
2013-04-11 14:03:05 +0000

    add script to generate the language template

---
+++ scripts/generate-template.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## script to generate the language gettext template.po from the source code
+
+svnup=$2
+reportto=$1
+
+[ "$reportto" ] || reportto=root@localhost 
+
+if [ "$svnup" ]; then
+  cd trunk
+  svn up
+  cd ..
+fi
+
+now=$(date +%Y%m%d%H%M)
+
+## from http://www.lxg.de/code/playing-with-xgettext
+echo '' > messages.po # xgettext needs that file, and we need it empty
+find . -type f -iname "*.php" | xgettext --omit-header --keyword=__ --keyword=_e --keyword=s --keyword=get -j -f -
+msgmerge -N phplist.pot messages.po > phplist-new.pot 2>/dev/null
+
+diff phplist-new.pot phplist.pot > diff${now}
+if [ -s "diff${now}" ]; then
+  exec > /tmp/message$$
+  echo Language text updates 
+  echo
+  fgrep '< msgid' diff${now} | sed s/'< msgid'//
+
+  mail -s "phpList language template update" $reportto < /tmp/message$$ 
+  rm -f diff${now} /tmp/message$$
+  mv -f phplist-new.pot phplist.pot
+fi
+rm -f messages.po phplist-new.pot diff{$now}
+
