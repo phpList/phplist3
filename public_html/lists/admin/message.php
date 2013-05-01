12faffe95959ff4868575b8dd418b2f96f7fe132
Michiel Dethmers <git@phplist.com>
2013-05-01 16:06:55 -0300

    show excluded lists

--- public_html/lists/admin/message.php
+++ public_html/lists/admin/message.php
@@ -114,7 +114,7 @@ if (ALLOW_ATTACHMENTS) {
  # print '</table>';
 }
 
-$content .= '<tr><td colspan="2"><h3>' . $GLOBALS['I18N']->get('Lists this message has been sent to') . ':</h3></td></tr>';
+$content .= '<tr><td colspan="2"><h4>' . $GLOBALS['I18N']->get('This campaign has been sent to subscribers, who are member of the following lists') . ':</h4></td></tr>';
 
 $lists_done = array();
 $result = Sql_Query("select l.name, l.id from $tables[listmessage] lm, $tables[list] l where lm.messageid = $id and lm.listid = l.id");
@@ -122,9 +122,16 @@ if (!Sql_Num_Rows($result))
   $content .= '<tr><td colspan="2">' . $GLOBALS['I18N']->get('None yet') . '</td></tr>';
 while ($lst = Sql_fetch_array($result)) {
   array_push($lists_done,$lst['id']);
-  $content .=sprintf ('<tr><td>%d</td><td>%s</td></tr>',$lst['id'],$lst['name']);
+  $content .= sprintf ('<tr><td>%d</td><td>%s</td></tr>',$lst['id'],$lst['name']);
 }
 
+if ($msgdata['excludelist']) {
+  $content .= '<tr><td colspan="2"><h4>' . $GLOBALS['I18N']->get('Except when they were also member of these lists') . ':</h4></td></tr>';
+  $result = Sql_Query(sprintf('select l.name, l.id from %s l where id in (%s)',$tables['list'],join(',',$msgdata['excludelist'])));
+  while ($lst = Sql_fetch_array($result)) {
+    $content .= sprintf ('<tr><td>%d</td><td>%s</td></tr>',$lst['id'],$lst['name']);
+  }
+}
 $content .= '</table>';
 
 $panel = new UIPanel($msgdata['subject'],$content);
