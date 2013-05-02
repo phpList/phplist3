e18a831b898a62b928475516ef0b1c300c8dd6cc
Michiel Dethmers <git@phplist.com>
2013-05-01 21:10:50 -0300

    add sample image for template preview

--- public_html/lists/admin/viewtemplate.php
+++ public_html/lists/admin/viewtemplate.php
@@ -19,7 +19,7 @@ if (empty($_GET["embed"])) {
   ob_end_clean();
 //BUGFIX 15292 - by tipichris - mantis.phplist.com/view.php?id=15292
  // print previewTemplate($id,$_SESSION["logindetails"]["id"],nl2br($GLOBALS['I18N']->get('Sample Newsletter text')));
- print previewTemplate($id,$_SESSION["logindetails"]["id"],$GLOBALS['I18N']->get('Sample Newsletter text'));
+ print previewTemplate($id,$_SESSION["logindetails"]["id"],'<h4 style="color:#369;">'.$GLOBALS['I18N']->get('Sample Newsletter Content').'</h4>'.'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a quam nec neque interdum venenatis. Duis sed lacus vel elit vehicula facilisis. Phasellus nec quam a justo dapibus iaculis fermentum vitae velit. Donec quis lorem sapien.</p><hr size=1 style="color:#69C" /><br /><p><b style="color:#369"> Donec iaculis ultrices volutpat? </b></p><p><img src="images/sampleimage.jpg" style="float:right;margin-left:10px;border:1px solid #999" />Etiam sodales tortor a sapien sagittis id blandit tortor lacinia. <a href="#" style="color:#369;text-decoration:underline">Integer in elit magna</a>. Phasellus vestibulum nulla ante. Etiam augue magna, venenatis ut ornare eget, tempor pulvinar urna. Maecenas molestie elementum leo vel vehicula. In sed porttitor ligula. Quisque vulputate tortor at tellus gravida in molestie ipsum cursus. Fusce posuere mauris at mauris feugiat quis volutpat velit vestibulum.</p>');
 //END BUGFIX
 }
 
