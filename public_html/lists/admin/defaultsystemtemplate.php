233ac579f66004ae8859af98d259bbe6704d8ee4
Michiel Dethmers <git@phplist.com>
2013-05-02 19:00:31 -0300

    template style update

--- public_html/lists/admin/defaultsystemtemplate.php
+++ public_html/lists/admin/defaultsystemtemplate.php
@@ -9,7 +9,7 @@ $template = '<div style="margin:0; text-align:center; width:100%; background:#EE
     <div style="width:96%;margin:0 auto; border-top:6px solid #369;border-bottom: 6px solid #369;background:#DEF;" >
         <h3 style="margin-top:5px;background-color:#69C; font-weight:normal; color:#FFF; text-align:center; margin-bottom:5px; padding:10px; line-height:1.2; font-size:21px; text-transform:capitalize;">[SUBJECT]</h3>
         <div style="text-align:justify;background:#FFF;padding:20px; border-top:2px solid #369;min-height:200px;font-size:13px; border-bottom:2px solid #369;">[CONTENT]<div style="clear:both"></div></div>
-        <div style="clear:both;background:#69C;font-weight:normal; padding:10px;color:#FFF;text-align:center;font-size:14px;margin:5px 0px">[FOOTER]<br/>[SIGNATURE]</div>
+        <div style="clear:both;background:#69C;font-weight:normal; padding:10px;color:#FFF;text-align:center;font-size:11px;margin:5px 0px">[FOOTER]<br/>[SIGNATURE]</div>
     </div>
 <br /></div>';
 
