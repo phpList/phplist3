a29c7768b58fbf98cf3df30ef8ae06564a82ffb2
Michiel Dethmers <git@phplist.com>
2013-05-07 16:19:14 -0300

    add call to re-initialise a plugin

--- public_html/lists/admin/actions/plugins.php
+++ public_html/lists/admin/actions/plugins.php
@@ -1,6 +1,6 @@
 <?php
 
-$status = 'OK';
+$status = 'FAIL';
 $disabled_plugins = unserialize(getConfig('plugins_disabled'));
 
 if (isset($_GET['disable'])) {
@@ -13,8 +13,16 @@ if (isset($_GET['disable'])) {
   if (isset($disabled_plugins[$_GET['enable']])) {
     unset($disabled_plugins[$_GET['enable']]);
   }
+  if (isset($GLOBALS['allplugins'][$_GET['enable']])) {
+     $GLOBALS['allplugins'][$_GET['enable']]->initialise();
+  }
+#  var_dump($disabled_plugins);
   saveConfig('plugins_disabled',serialize($disabled_plugins),0);
   $status = $GLOBALS['img_tick'];
+} elseif (isset($_GET['initialise'])) {
+  if (isset($GLOBALS['plugins'][$_GET['initialise']])) {
+    $status = $GLOBALS['plugins'][$_GET['initialise']]->initialise();
+  }
 }
 #var_dump($_GET);
 
