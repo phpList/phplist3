982106caf6ef06a7dd76f367a51236fb561e977b
Michiel Dethmers <git@phplist.com>
2013-05-13 21:24:15 -0300

    add simple system page

---
+++ public_html/lists/admin/system.php
@@ -0,0 +1,34 @@
+<?php
+
+## some system tasks, that don't need to be in the menu
+
+/*
+ * bit of code to list all files and compare them to the menu categorisation
+ *
+$inmenu = array();
+foreach ($GLOBALS['pagecategories'] as $category => $cat_details) {
+  $inmenu = array_merge($inmenu,$cat_details['menulinks']);
+}
+var_dump($inmenu);
+
+$dir = opendir(dirname(__FILE__));
+while ($file = readdir($dir)) {
+  if ($file != '.' && $file != '..' && preg_match('/\.php$/',$file) && !in_array(basename($file,'.php'),$inmenu)) {
+    print $file.'<br/>';
+  }
+}
+*/
+
+$pages = array(
+  'initialise',
+  'upgrade',
+  'dbcheck',
+  'reindex',
+  'converttoutf8',
+);
+
+print '<ul>';
+foreach ($pages as $page) {
+  print '<li>'.PageLink2($page,$page).'</li>';
+}
+print '</ul>';
