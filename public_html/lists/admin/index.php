8fbac01055e551aa33fb3d208abc16e3dae8576e
Michiel Dethmers <git@phplist.com>
2013-05-18 01:12:30 -0300

    place setup reminder on the right

--- public_html/lists/admin/index.php
+++ public_html/lists/admin/index.php
@@ -391,7 +391,7 @@ if ($GLOBALS["require_login"] && $page != "login") {
   if ($page != "logout" && empty($logoutontop) && !$ajax) {
   #  print '<div class="right">'.PageLink2("logout",$GLOBALS['I18N']->get('logout')).'</div>';
     if (!empty($_SESSION['firstinstall']) && $page != 'setup') {
-      print '<div class="info right">'.PageLinkClass("setup",$GLOBALS['I18N']->get('Continue Configuration'),'','firstinstallbutton').'</div>';
+      print '<div class="fright">'.PageLinkClass("setup",$GLOBALS['I18N']->get('Continue Configuration'),'','firstinstallbutton').'</div>';
     }
   }
 }
