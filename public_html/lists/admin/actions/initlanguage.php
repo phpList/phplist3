6916e2ff1df7a08d399c8dd53e0d48acf62a2ba9
Michiel Dethmers <git@phplist.com>
2013-05-29 10:06:00 -0300

    add some delay to avoid flashing on the screen too quickly

--- public_html/lists/admin/actions/initlanguage.php
+++ public_html/lists/admin/actions/initlanguage.php
@@ -1,7 +1,12 @@
 <?php
 
+$now = time();
 
 $status = '';
 $GLOBALS['I18N']->initFSTranslations();
+
+if (time() - $now < 4) {
+  sleep(4);
+}
 
 $status = '<script type="text/javascript">$("#dialog").dialog(\'close\'); document.location = document.location; </script>';
