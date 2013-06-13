fea27d9ab4b69fc81cb69f20c1185b0ef9eaf4a4
Michiel Dethmers <git@phplist.com>
2013-06-13 18:22:19 -0300

    add ajax actions for user page

---
+++ public_html/lists/admin/actions/user.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!defined('PHPLISTINIT')) die();
+
+if (isset($_GET['id'])) {
+  $userid = sprintf('%d',$_GET['id']);
+}
+
+if (empty($userid)) {
+  return;
+}
+
+if (!empty($_GET['blacklist'])) {
+  $email = Sql_Fetch_Row_Query(sprintf('select email from %s where id = %d',$GLOBALS['tables']['user'],$userid));
+  if (!empty($email[0])) {
+    addUserToBlackList($email[0],s('Manually blacklisted by %s',$_SESSION['logindetails']['adminname']));
+    $status = 'OK';
+  }
+}
