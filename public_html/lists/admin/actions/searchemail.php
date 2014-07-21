fdcd75da5d24475f1c02803296b026eee8e8712c
Michiel Dethmers <git@phplist.com>
2014-07-21 10:49:58 +0100

    block use of unfinished code

--- public_html/lists/admin/actions/searchemail.php
+++ public_html/lists/admin/actions/searchemail.php
@@ -2,6 +2,8 @@
 
 //[ { "id": "Alcedo atthis", "label": "Common Kingfisher", "value": "Common Kingfisher" }, { "id": "Haliaeetus leucoryphus", "label": "Pallasâ€™s Fish Eagle", "value": "Pallasâ€™s Fish Eagle" }, { "id": "Ceryle alcyon", "label": "Belted Kingfisher", "value": "Belted Kingfisher" } ]
 
+exit; ## unfinished code
+
 $req = Sql_Query(sprintf('select id,email as label, email as value from %s where email like "%s%%" limit 10',$tables['user'],$_GET['term']));
 $results = array();
 while ($row = Sql_Fetch_Assoc($req)) {
