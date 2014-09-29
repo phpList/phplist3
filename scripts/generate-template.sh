5486c36c17474890ba6e2bc9e26d1c60ad647c6b
Rodrigo Campos <rodrigo@sdfg.com.ar>
2014-09-29 14:09:36 -0300

    scripts/generate-template.sh: Move mail-reporting logic to a function
    This makes the script more readable as a whole.

--- scripts/generate-template.sh
+++ scripts/generate-template.sh
@@ -14,7 +14,25 @@ fi
 
 [ -d public_html ] || exit 1; ## needs to run from phplist root
 
-now=$(date +%Y%m%d%H%M)
+function mail_template_diff() {
+	# TODO: Check if there is a way to make this even simpler ?
+	now=$(date +%Y%m%d%H%M)
+
+	diff phplist-new.pot $current > diff${now}
+	fgrep '< msgid' diff${now} | sed s/'< msgid'// > diff2${now}
+
+	if [ -s "diff2${now}" ]; then
+		exec > /tmp/message$$
+		echo These are this weeks changes in the language template file
+		echo They will show up in http://translation.phplist.com as untranslated
+		echo Please update your translations, thanks
+		echo
+		cat diff2${now}
+
+		mail -s "phpList language changes" $reportto < /tmp/message$$
+		rm -f diff${now} diff2${now} /tmp/message$$
+	fi
+}
 
 ## from http://www.lxg.de/code/playing-with-xgettext
 echo '' > messages.po # xgettext needs that file, and we need it empty
@@ -25,20 +43,8 @@ php scripts/structuredump.php > public_html/databasestructure.php
 find public_html -type f -iname "*.php" | xgettext --omit-header --keyword=__ --keyword=_e --keyword=s --keyword=get -j -f -
 msgmerge -qN $current messages.po > phplist-new.pot
 
-diff phplist-new.pot $current > diff${now}
-fgrep '< msgid' diff${now} | sed s/'< msgid'// > diff2${now}
+mail_template_diff
 
-if [ -s "diff2${now}" ]; then
-  exec > /tmp/message$$
-  echo These are this weeks changes in the language template file
-  echo They will show up in http://translation.phplist.com as untranslated
-  echo Please update your translations, thanks 
-  echo
-  cat diff2${now}
-
-  mail -s "phpList language changes" $reportto < /tmp/message$$
-  rm -f diff${now} diff2${now} /tmp/message$$
-fi
 mv -f phplist-new.pot phplist.pot
-rm -f messages.po phplist-new.pot diff${now} diff2${now} public_html/databasestructure.php
+rm -f messages.po phplist-new.pot public_html/databasestructure.php
 
