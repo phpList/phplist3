ecfd18639d1435427e028de7dd471d6fe604890e
Michiel Dethmers <git@phplist.com>
2014-10-08 16:34:09 +0100

    #17318 - only show default login info on phplist demo site

--- public_html/lists/admin/login.php
+++ public_html/lists/admin/login.php
@@ -1,7 +1,7 @@
 <?php
 require_once dirname(__FILE__).'/accesscheck.php';
 
-if (TEST) {
+if (TEST && strpos($_SERVER['HTTP_HOST'],'phplist.com') !== false) {
   print Info($GLOBALS['I18N']->get('default login is')." admin, ".$GLOBALS['I18N']->get('with password')." phplist").'.';
 }
 
