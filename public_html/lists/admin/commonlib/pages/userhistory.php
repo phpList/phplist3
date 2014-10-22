6552f3152bba279c0b21a6fe1c62fccd18c3bdce
Michiel Dethmers <git@phplist.com>
2014-10-22 16:34:38 +0100

    add reverse order

--- public_html/lists/admin/commonlib/pages/userhistory.php
+++ public_html/lists/admin/commonlib/pages/userhistory.php
@@ -72,7 +72,7 @@ if (Sql_Affected_Rows()) {
 $ls = new WebblerListing($GLOBALS['I18N']->get('Messages'));
 if (Sql_Table_Exists($tables["usermessage"])) {
   $msgs = Sql_Query(sprintf('select messageid,entered,viewed,(viewed = 0 or viewed is null) as notviewed,
-    abs(unix_timestamp(entered) - unix_timestamp(viewed)) as responsetime from %s where userid = %d and status = "sent"',$tables["usermessage"],$user["id"]));
+    abs(unix_timestamp(entered) - unix_timestamp(viewed)) as responsetime from %s where userid = %d and status = "sent" order by entered desc',$tables["usermessage"],$user["id"]));
   $num = Sql_Affected_Rows();
 } else {
   $num = 0;
