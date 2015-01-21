e52e346c04dc08c74f1eb308a4fd06dc4081697e
Michiel Dethmers <git@phplist.com>
2015-01-21 19:45:41 +0000

    #17585 - remove forwardform placeholder

--- public_html/lists/admin/sendemaillib.php
+++ public_html/lists/admin/sendemaillib.php
@@ -185,7 +185,7 @@ function sendEmail ($messageid,$email,$hash,$htmlpref = 0,$rssitems = array(),$f
   $url = getConfig("forwardurl");
 
   # make sure there are no newlines, otherwise they get turned into <br/>s
-  $html["forwardform"] = sprintf('<form method="get" action="%s" name="forwardform" class="forwardform"><input type="hidden" name="uid" value="%s" /><input type="hidden" name="mid" value="%d" /><input type="hidden" name="p" value="forward" /><input type=text name="email" value="" class="forwardinput" /><input name="Send" type="submit" value="%s" class="forwardsubmit"/></form>',$url,$hash,$messageid,$GLOBALS['strForward']);
+  $html["forwardform"] = '';#sprintf('<form method="get" action="%s" name="forwardform" class="forwardform"><input type="hidden" name="uid" value="%s" /><input type="hidden" name="mid" value="%d" /><input type="hidden" name="p" value="forward" /><input type=text name="email" value="" class="forwardinput" /><input name="Send" type="submit" value="%s" class="forwardsubmit"/></form>',$url,$hash,$messageid,$GLOBALS['strForward']);
   $text["signature"] = "\n\n-- powered by phpList, www.phplist.com --\n\n";
   $url = getConfig("preferencesurl");$sep = strpos($url,'?') === false ? '?':'&';
   $html["preferences"] = sprintf('<a href="%s%suid=%s">%s</a>',$url,htmlspecialchars($sep),$hash,$strThisLink);
