417340d7152114cb34b1ca92282855addcf87333
Michiel Dethmers <git@phplist.com>
2015-03-24 13:50:53 +0000

    #17569 - set the encryption algorithm for passwords

--- public_html/lists/config/config.php
+++ public_html/lists/config/config.php
@@ -107,3 +107,7 @@ $bounce_mailbox_purge_unprocessed = 1;
 # how many bounces in a row need to have occurred for a user to be marked unconfirmed
 $bounce_unsubscribe_threshold = 5;
 
+# choose the encryption method for password
+# check the extended config for more info
+# in most cases, it is fine to leave this as it is
+define("ENCRYPTION_ALGO",'sha256');
