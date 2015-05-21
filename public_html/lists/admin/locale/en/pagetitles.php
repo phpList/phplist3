168b890ca2102e82dc1caef8f78412fa725bb608
Michiel Dethmers <git@phplist.com>
2015-05-21 16:26:00 +0100

    #17582 - remove use of "view"

--- public_html/lists/admin/locale/en/pagetitles.php
+++ public_html/lists/admin/locale/en/pagetitles.php
@@ -40,8 +40,8 @@ switch ($page) {
   case 'admins': $page_title = 'List administrators';break;
   case 'adminattributes': $page_title = 'Configure administrator attributes';break;
   case 'processbounces': $page_title = 'Retrieve bounces from server';break;
-  case 'bounces': $page_title = 'View bounces';break;
-  case 'bounce': $page_title = 'View a bounce';break;
+  case 'bounces': $page_title = 'Bounces';break;
+  case 'bounce': $page_title = 'Bounce';break;
   case 'spageedit': $page_title = 'Edit a subscribe page';break;
   case 'spage': $page_title = 'Subscribe pages';break;
   case 'eventlog': $page_title = 'Log of events';break;
