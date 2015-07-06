118837d928df538538326b8e59ea6b10539bbe18
Michiel Dethmers <git@phplist.com>
2015-07-06 17:13:56 +0100

    #17724 - respect the order of attribute values in the subscriber editing backend

--- public_html/lists/admin/inc/userlib.php
+++ public_html/lists/admin/inc/userlib.php
@@ -452,7 +452,7 @@ function UserAttributeValueSelect($user = 0,$attribute = 0) {
   $value = UserAttributeValue($user,$attribute);
 #  $html = 'Value: '.$value;
   $html = sprintf('<select name="attribute[%d]" style="attributeinput" >',$attribute);
-  $res = Sql_Query("select id,name from $table_prefix"."listattr_".$att["tablename"]." order by name");
+  $res = Sql_Query("select id,name from $table_prefix"."listattr_".$att["tablename"]." order by listorder,name");
   if (!Sql_Affected_Rows())
     return "(No values available)";
   $html .= '<option value="0">-- no value</option>';
@@ -526,6 +526,7 @@ function is_email($email) {
     ) {
     return 0;
   }
+  
   $tlds = getConfig('internet_tlds');
   if (empty($tlds)) {
     $tlds = 'ac|ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|asia|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cat|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dev|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|home|hr|ht|hu|id|ie|il|im|in|info|int|io|iq|ir|is|it|jm|je|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|loc|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tel|tf|tg|th|tj|tk|tm|tn|to|tp|tr|travel|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw';
