51e49a0499eec4902eda3500611cd89d29bbd253
Duncan Cameron <phplist@dcameron.me.uk>
2015-09-27 10:01:00 +0100

    Allow plugins to modify the loaded message

--- public_html/lists/admin/defaultplugin.php
+++ public_html/lists/admin/defaultplugin.php
@@ -459,6 +459,17 @@ class phplistPlugin {
     return '';
   }
   
+  /* 
+   * Allows plugins to modify the fields of the loaded message.
+   * Those fields will then be customised for each subscriber
+   *
+   * @param integer $messageid: ID of the message
+   * @param array   &$message: associative array of message data
+   * @return void
+   */
+  public function processLoadedMessage($messageid, array &$message)
+  {
+  }
   
   /* 
    * parseOutgoingTextMessage
