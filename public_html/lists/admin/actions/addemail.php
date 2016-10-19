61a8e3b26a69f0fb5f9f99111dad99c5d0414f9f
Michiel Dethmers <git@phplist.com>
2016-10-19 20:13:49 +0100

    add uuid when creating test subscriber

--- public_html/lists/admin/actions/addemail.php
+++ public_html/lists/admin/actions/addemail.php
@@ -14,8 +14,8 @@ if (!empty($_GET['email'])) {
         $status = s('That is not a valid email address');
     } elseif ($delay > ADD_EMAIL_THROTTLE) {
         $_SESSION['last_addemail'] = time();
-        Sql_Query(sprintf('insert into %s (email,uniqid,htmlemail,entered) values("%s","%s",1,now())',
-            $GLOBALS['tables']['user'], sql_escape($_GET['email']), getUniqid()), 1);
+        Sql_Query(sprintf('insert into %s (email,uniqid,htmlemail,entered,uuid) values("%s","%s",1,now(),"%s")',
+            $GLOBALS['tables']['user'], sql_escape($_GET['email']), getUniqid(),uuid::generate(4)), 1);
         addUserHistory($_GET['email'], s('Added by %s', adminName()), s('Added with add-email on test'));
         $status = s('Email address added');
     } else {
