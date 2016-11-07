2a124018ce96bf26b754edebb5470e699fbbab24
Sam Tuke <mail@samtuke.com>
2016-11-07 22:15:19 +0100

    Set column title independently of table title and removed table title

--- public_html/lists/admin/statsoverview.php
+++ public_html/lists/admin/statsoverview.php
@@ -87,7 +87,7 @@ if (!$id) {
         print '<p class="information">' . $GLOBALS['I18N']->get('There are currently no campaigns to view') . '</p>';
     }
 
-    $ls = new WebblerListing('Campaign statistics');
+    $ls = new WebblerListing('');
     $ls->usePanel($paging);
     while ($row = Sql_Fetch_Array($req)) {
         #  $element = '<!--'.$row['messageid'].'-->'.shortenTextDisplay($row['subject'],30);
@@ -104,6 +104,7 @@ if (!$id) {
 
         $ls->addElement($element,
             PageURL2('statsoverview&amp;id=' . $row['messageid']));#,PageURL2('message&amp;id='.$row['messageid']));
+        $ls->setElementHeading($GLOBALS['I18N']->get('Campaign'));
         $ls->setClass($element, 'row1');
         #   $ls->addColumn($element,$GLOBALS['I18N']->get('owner'),$row['owner']);
         $ls->addColumn($element, $GLOBALS['I18N']->get('sent'), $row['total']);
