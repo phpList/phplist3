634564438647fcb6867405461ebf1babe03f1e79
mariez <marielazarate@gmail.com>
2016-11-18 18:36:40 -0300

    add html list styles to " manage bounces " page

--- public_html/lists/admin/bouncemgt.php
+++ public_html/lists/admin/bouncemgt.php
@@ -2,8 +2,10 @@
 
 require_once dirname(__FILE__) . '/accesscheck.php';
 
-$spb = '<div>';
-$spe = '</div>';
+$spb = '<li>';
+$spe = '</li>';
+
+print '<ul style="list-style:none" class="navigation_list">';
 
 print $spb . PageLink2('bouncerules', $GLOBALS['I18N']->get('List Bounce Rules')) . $spe;
 print $spb . PageLink2('bounces', $GLOBALS['I18N']->get('View Bounces')) . $spe;
@@ -12,10 +14,12 @@ print $spb . PageLink2('checkbouncerules', $GLOBALS['I18N']->get('Check Current 
 
 print $spb . PageLink2('processbounces', $GLOBALS['I18N']->get('Process Bounces')) . $spe;
 
+print '</ul><br />';
+
 $numrules = Sql_Fetch_Row_Query(sprintf('select count(*) from %s', $GLOBALS['tables']['bounceregex']));
 if (!$numrules[0]) {
-    print '<p class="information">' . $GLOBALS['I18N']->get('You currently have no rules defined.      You can click "Generate Bounce Rules" in order to auto-generate rules from your existing bounces.      This will results in a lot of rules which you will need to review and activate.      It will however, not catch every single bounce, so it will be necessary to add new rules over      time when new bounces come in.') . '</p>';
+    print '<p class="information text-info"><big>' . $GLOBALS['I18N']->get('You currently have no rules defined.      You can click "Generate Bounce Rules" in order to auto-generate rules from your existing bounces.      This will results in a lot of rules which you will need to review and activate.      It will however, not catch every single bounce, so it will be necessary to add new rules over      time when new bounces come in.') . '</big></p>';
 } else {
-    print '<p class="information">' . $GLOBALS['I18N']->get('You have already defined bounce rules in your system.      Be careful with generating new ones, because these may interfere with the ones that exist.') . '</p>';
+    print '<p class="information text-warning"><big>' . $GLOBALS['I18N']->get('You have already defined bounce rules in your system.      Be careful with generating new ones, because these may interfere with the ones that exist.') . '</big></p>';
 }
-print '<p class="button">' . PageLink2('generatebouncerules', $GLOBALS['I18N']->get('Generate Bounce Rules')) . '</p>';
+print '<br /><p class="button">' . PageLink2('generatebouncerules', $GLOBALS['I18N']->get('Generate Bounce Rules')) . '</p>';
