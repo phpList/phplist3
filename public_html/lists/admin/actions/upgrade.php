d20c5f9a0920e6ce4a6fca2ab78120ddd5909c25
Michiel Dethmers <git@phplist.com>
2016-11-24 22:20:12 +0000

    add counter of current number of TLDs

--- public_html/lists/admin/actions/upgrade.php
+++ public_html/lists/admin/actions/upgrade.php
@@ -7,6 +7,8 @@ verifyCsrfGetToken();
 
 if ($_GET['update'] == 'tlds') {
     refreshTlds(true);
+    $tlds = explode('|',getConfig('internet_tlds'));
 }
 
 $status = s('Top level domains were updated successfully');
+$status .= '<br/>'.s('%d Top Level Domains',count($tlds));
