8926e9503733d8503e794eb1643d3c0be77fc0fa
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2017-03-28 11:12:45 +0200

    Update the message about requirement too

--- public_html/lists/admin/index.php
+++ public_html/lists/admin/index.php
@@ -492,7 +492,7 @@ if (!$ajax && $page != 'login') {
         echo Info($GLOBALS['I18N']->get('Running in testmode, no emails will be sent. Check your config file.'));
     }
     if (version_compare(PHP_VERSION, '5.3.3', '<') && WARN_ABOUT_PHP_SETTINGS) {
-        Error(s('Your PHP version is out of date. phpList requires PHP version 5.4.0 or higher.'));
+        Error(s('Your PHP version is out of date. phpList requires PHP version 5.3.3 or higher.'));
     }
     if (defined('RELEASEDATE') && ((time() - RELEASEDATE) / 31536000) > 2) {
         Fatal_Error(s('Your phpList version is older than two years. Please %supgrade phpList</a> before continuing.</br>
