978cb0f7b20a894308e7537fdbd5f751a9bdefd3
jd440 <jean.dufond@hotmail.fr>
2017-10-21 15:23:41 +0700

    correct comment, and move setting to config.php

--- public_html/lists/config/config_extended.php
+++ public_html/lists/config/config_extended.php
@@ -177,10 +177,6 @@ define('UNSUBSCRIBE_REQUIRES_PASSWORD', 0);
 // the default way, which will ask them for a reason, set this to 1
 define('UNSUBSCRIBE_JUMPOFF', 0);
 
-// To not send confirmation of unsubscription , instead of
-// the default way, which will send it, set this to 0
-define('UNSUBSCRIBE_CONFIRMATION', false);
-
 // when a user unsubscribes they are sent one final email informing them of
 // their unsubscription. In order for that email to actually go out, a gracetime
 // needs to be set otherwise it will never go out. The default of 5 minutes should
