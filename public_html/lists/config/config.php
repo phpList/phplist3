671c75b4aafa2583de9c9e0afc8ef454606426df
Sam Tuke <mail@samtuke.com>
2017-12-11 13:47:05 +0100

    Reverted mistaken inclusion of this file in a past commit

--- public_html/lists/config/config.php
+++ public_html/lists/config/config.php
@@ -15,31 +15,17 @@
 
 */
 
-$require_login = 0;
-
-#$language_module = "german.inc";
-
-$ui = 'phplist-ui-bootlist';
-
-define('USE_LIST_EXCLUDE', 1);
-
-$developer_email = 'sam@phplist.com';
-
-xdebug_disable();
-$GLOBALS['show_dev_errors'] = false;
-
 // what is your Mysql database server hostname
 $database_host = 'localhost';
 
 // what is the name of the database we are using
-//$database_name = 'pl';
 $database_name = 'phplistdb';
 
 // what user has access to this database
-$database_user = 'root';
+$database_user = 'phplist';
 
 // and what is the password to login to control the database
-$database_password = 'doge1917';
+$database_password = 'phplist';
 
 // if you have an SMTP server, set it here. Otherwise it will use the normal php mail() function
 //# if your SMTP server is called "smtp.mydomain.com" you enter this below like this:
@@ -52,7 +38,7 @@ define('PHPMAILERHOST', '');
 // this is here, to make sure you edited the config file and mails are not sent "accidentally"
 // on unmanaged systems
 
-define('TEST', 0);
+define('TEST', 1);
 
 /*
 
