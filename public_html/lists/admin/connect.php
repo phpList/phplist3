0c8b44c7eaac9a1df0ab554ba239e7bda7d7cf29
Sam Tuke <mail@samtuke.com>
2018-05-09 21:18:24 +0200

    Reformatted SQL query for readability

--- public_html/lists/admin/connect.php
+++ public_html/lists/admin/connect.php
@@ -2114,7 +2114,14 @@ function secs2time($secs)
 function listPlaceHolders()
 {
     $html = '<table border="1"><tr><td><strong>'.s('Attribute').'</strong></td><td><strong>'.s('Placeholder').'</strong></td></tr>';
-    $req = Sql_query('select name from '.$GLOBALS['tables']['attribute'].' order by listorder');
+    $req = Sql_query('
+    select 
+        name 
+    from 
+        '.$GLOBALS['tables']['attribute'].' 
+    order by 
+        listorder
+    ');
     while ($row = Sql_Fetch_Row($req)) {
         if (strlen($row[0]) <= 30) {
             $html .= sprintf('<tr><td>%s</td><td>[%s]</td></tr>', $row[0], strtoupper(cleanAttributeName($row[0])));
