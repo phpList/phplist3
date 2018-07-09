4b8291e19194387a6bb3a9fd76497a67993cbe4d
Xheni Myrtaj <myrtajxheni@gmail.com>
2018-07-09 16:17:44 +0200

    change copy to draft icon (#369)
    Signed-off-by: Xheni Myrtaj <myrtajxheni@gmail.com>

--- public_html/lists/admin/messages.php
+++ public_html/lists/admin/messages.php
@@ -626,7 +626,7 @@ END;
         }
 
         if ($msg['status'] == 'sent') {
-            $actionbuttons .= '<span class="edit">'.PageLinkButton('messages', s('Copy to Draft'),
+            $actionbuttons .= '<span class="copy">'.PageLinkButton('messages', s('Copy to Draft'),
                     'tab=draft&duplicate='.$msg['id'], '', s('Copy to Draft')).'</span>';
         }
 
