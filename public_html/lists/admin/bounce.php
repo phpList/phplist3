e862e7372063702844652f75b1c518f6f462b562
Xheni Myrtaj <xheni@protonmail.com>
2018-10-31 11:56:01 +0100

    Better warning message for bounce that doesn't longer exist (#430)
    * better message display
    
    Signed-off-by: Xheni Myrtaj <myrtajxheni@gmail.com>
    
    * bounce doesn't exist warning
    
    Signed-off-by: Xheni Myrtaj <myrtajxheni@gmail.com>
    
    * display button for the bounces page
    
    Signed-off-by: Xheni Myrtaj <myrtajxheni@gmail.com>

--- public_html/lists/admin/bounce.php
+++ public_html/lists/admin/bounce.php
@@ -135,7 +135,10 @@ $guessedemail = '';
 if ($id) {
     $result = Sql_query("SELECT * FROM {$tables['bounce']} where id = $id");
     if (!Sql_Affected_Rows()) {
-        Fatal_Error($GLOBALS['I18N']->get('This bounce no longer exists in the database.'));
+
+        echo '<div class="alert alert-warning " role="alert">
+        '.s('This bounce no longer exists in the database.').'</div>';
+        echo '<div class="pull-left">'.PageLinkButton('bounces', s('Manage bounces')).'</div>';
 
         return;
     }
