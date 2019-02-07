0d73ed1e8f7441a7d9779d1905d39ad7a9260946
Xheni Myrtaj <myrtajxheni@gmail.com>
2019-02-07 15:54:48 +0100

    Text placeholder
    Signed-off-by: Xheni Myrtaj <myrtajxheni@gmail.com>

--- public_html/lists/admin/lib.php
+++ public_html/lists/admin/lib.php
@@ -2156,10 +2156,29 @@ function parseLogoPlaceholders($content)
     return $content;
 }
 
-function parseVCardPlaceholder($content) {
+/**
+ * Parse [CONTACT] as HTML placeholder for VCard
+ * @param $content
+ * @return mixed
+ */
+function parseVCardHTMLPlaceholder($content) {
     preg_match_all('/\[CONTACT\:?(\d+)?\]/', $content, $contactInstances);
     foreach ($contactInstances[0] as $index => $contactInstance) {
-        $content = str_replace($contactInstance, '<a href="'.htmlentities(getConfig('vcardurl')).' ">'.$GLOBALS['$strContactMessage'].'</a>',$content);
+        $content = str_replace($contactInstance, '<a href="'.htmlentities(getConfig('vcardurl')).' ">'.$GLOBALS['strContactMessage'].'</a>', $content);
+    }
+
+    return $content;
+}
+
+/**
+ * Parse [CONTACT] as Text placeholder for VCard
+ * @param $content
+ * @return mixed
+ */
+function parseVCardTextPlaceholder($content) {
+    preg_match_all('/\[CONTACT\:?(\d+)?\]/', $content, $contactInstances);
+    foreach ($contactInstances[0] as $index => $contactInstance) {
+        $content = str_replace($contactInstance, $GLOBALS['strContactMessage'].' '.htmlentities(getConfig('vcardurl')), $content);
     }
 
     return $content;
