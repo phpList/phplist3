b232e1cf0c4e1e79750f414a0429ddb8439b7bb7
Michiel Dethmers <git@phplist.com>
2019-02-10 13:22:24 +0000

    avoid processing a single bounce twice

--- public_html/lists/admin/actions/reconcileusers.php
+++ public_html/lists/admin/actions/reconcileusers.php
@@ -68,7 +68,7 @@ if ($_GET['option'] == 'deleteinvalidemail') {
     $status .= $c.' '.s('subscribers deleted')."<br/>\n";
 } elseif ($_GET['option'] == 'relinksystembounces') {
     $status = s('Relinking transactional messages that bounced to the related subscriber profile').'<br/ >';
-    $req = Sql_Query(sprintf('select count(id) from %s where status = "bounced system message" and comment like "%% marked unconfirmed"',$tables['bounce']));
+    $req = Sql_Query(sprintf('select count(id) from %s b where b.status = "bounced system message" and b.comment like "%% marked unconfirmed" and id not in (select distinct bounce from %s)',$tables['bounce'],$tables['user_message_bounce']));
     $totalRow = Sql_Fetch_Row($req);
     $total = $totalRow[0];
     $status .= s('%d to process',$total).'<br/>';
@@ -76,13 +76,16 @@ if ($_GET['option'] == 'deleteinvalidemail') {
 
     $cnt = 0;
     $done = 0;
-    $req = Sql_Query(sprintf('select id,comment,date from %s where status = "bounced system message" and comment like "%% marked unconfirmed"',$tables['bounce']));
+    $req = Sql_Query(sprintf('select id,comment,date from %s where status = "bounced system message" and comment like "%% marked unconfirmed" and id not in (select distinct bounce from %s)',$tables['bounce'],$tables['user_message_bounce']));
     while ($row = Sql_Fetch_Assoc($req)) {
         ++$cnt;
         set_time_limit(60);
         if (preg_match('/([\d]+) marked unconfirmed/',$row['comment'],$regs)) {
-            ++$done;
-            Sql_Query(sprintf('insert into %s (user,message,bounce,time) values(%d,-1,%d,current_timestamp)',$tables['user_message_bounce'],$regs[1],$row['id'],$row['date']));
+            $exists = Sql_Fetch_Row_Query(sprintf('select count(*) from %s where user = %d and message = -1 and bounce = %d',$tables['user_message_bounce'],$regs[1],$row['id']));
+            if (empty($exists[0])) {
+                Sql_Query(sprintf('insert into %s (user,message,bounce,time) values(%d,-1,%d,"%s")',$tables['user_message_bounce'],$regs[1],$row['id'],$row['date']));
+                ++$done;
+            }
         }
     }
     $status .= s('%d bounces have been associated with a subscriber profile',$done);
