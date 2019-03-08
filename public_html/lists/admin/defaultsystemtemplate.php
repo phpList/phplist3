3304e54f777943f6841e23a7619e5cb0601247ce
SuelaP <suela_palushi@hotmail.com>
2019-03-08 13:50:28 +0100

    Replace existing templates, change names to be descriptive

--- public_html/lists/admin/defaultsystemtemplate.php
+++ public_html/lists/admin/defaultsystemtemplate.php
@@ -11,104 +11,400 @@ $systemtemplate = '<div style="margin:0; text-align:center; width:100%; backgrou
         <div style="clear:both;background:#69C;font-weight:normal; padding:10px;color:#FFF;text-align:center;font-size:11px;margin:5px 0px">[FOOTER]<br/>[SIGNATURE]</div>
     </div>
 <br /></div>';
-$template1 = '<div style="max-width:800px;width:100%;background:#FFF;display:block;margin:0px auto;font-family: Arial,sans-serif;text-align:center;padding:30px 0">
 
-<table style="width:100%;" cellpadding=0 cellspacing=0>
-<tbody>
-<tr>
-<td style="text-align:left;width:70%">
-    <div style="display:inline-block;max-width:100%;font-size:30px;margin-bottom:10px"><a href="[WEBSITE]" style="color:#FF9900;text-decoration:none"><img src="[LOGO:400]" style="width:100%" alt="NEWSLETTER" /></a></div>
-</td>
-<td style="text-align:right;padding-bottom:6px" valign="bottom">
-    <div style="width:30px;height:30px;background:#FF9900;display:inline-block;margin-right:7px;margin-bottom:0px;float:right"></div> 
-    <div style="width:8px;height:8px;background:#000000;display:inline-block;margin-right:7px;margin-bottom:7px"></div><br /> 
-    <div style="width:15px;height:15px;background:#000000;display:inline-block;margin-right:7px;"></div> 
-</td>
-</tr>
-</tbody>
-</table>
+$template1 = '<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div id="shell" style="margin: 0px; background: none repeat scroll 0% 0% rgb(235, 235, 235); color: rgb(102, 102, 102); padding: 0px; text-align: center; width: 100%;"><noscript>
+	<table cellspacing="0" border="0" class="required" style="margin: 0px auto;" width="600" cellpadding="10">
+		<tr>
+			<td align="right" style="font-size: 14px; font-family: Arial; line-height: 1.3em; text-align: left;">
+				<p style="font-size: 14px; margin: 10px; line-height: 1.3em; font-family: Arial; padding: 0px; text-align: right;"><a href="[PREFERENCESURL]" title="Change subscription options" style="color: #4A4A4A; text-decoration: none;">Change Subscription</a> : <a href="[UNSUBSCRIBEURL]" title="Unsubscribe" style="color: #4A4A4A; text-decoration: none;">Unsubscribe</a></p>
+			</td>
+		</tr>
+	</table>
+	</noscript>
+<table cellpadding="0" cellspacing="0" id="Box" style="margin: 10px auto; background: none repeat scroll 0% 0% rgb(255, 255, 255); color: rgb(102, 102, 102); padding: 0pt;" width="580">
+	<tbody>
+		<tr>
+			<td style="font-size: 14px; line-height: 1.3em; font-family: Arial; text-align: left;">
+			<table border="0" cellpadding="0" cellspacing="0" id="Header" style="background: none repeat scroll 0% 0% rgb(255, 255, 255);" width="600">
+				<tbody>
+					<tr valign="top">
+						<td style="font-size: 14px; line-height: 1.3em; font-family: Arial; text-align: left;"><a href="[WEBSITE]" title="[ORGANISATION_NAME]"><img alt="***" src="[LOGO:290]" style="display: block;margin-left: auto;margin-right: auto;" /></a></td>
+					</tr>
+				</tbody>
+			</table>
+			<!--content table starts-->
 
-<table style="background:#3366FF;color:#FFFFFF;width:100%;" cellpadding=0 cellspacing=0>
-<tbody>
-<tr>
-    <td rowspan="2" style="width:15px;padding:0;margin:0;background:#FFF;border-left:7px solid #3366FF;height:150px"></td>
-    <td rowspan="2" style="width:6px;padding:0;margin:0;background:#FFF;border-left:7px solid #3366FF;height:150px"></td>
-	<td style="text-align:right;vertical-align:top;padding-top:7px" valign="top;"> 
-        <div style="width:8px;height:8px;background:#FFFFFF;display:inline-block;margin-right:7px;float:right"></div> 
-        <div style="width:15px;height:15px;background:#FFFFFF;display:inline-block;margin-right:7px;float:right"></div> 
-	</td>
-</tr>
-<tr>
-	<td style="padding:10px 20px;text-align:right;font-size:30px;vertical-align:bottom" valign="bottom">[SUBJECT]</td>
-</tr>
-</tbody>
-</table>
+			<table border="0" cellpadding="0" cellspacing="0" class="content" style="background-color: rgb(255, 255, 255); color: rgb(102, 102, 102);" width="600">
+				<tbody>
+					<tr valign="top">
+						<td style="font-size: 14px; line-height: 1.3em; font-family: Arial; text-align: left;"><img alt="" height="20" src="pad.png" style="border: 0px none;" width="20" /></td>
+						<td class="maincontent" style="font-size: 14px; font-family: Arial; line-height: 1.3em; text-align: left;">
+						<h1 style="font-size: 1.8em; margin: 1em 0pt; color: rgb(87, 164, 210); padding: 0pt; letter-spacing: -1px;">[SUBJECT]</h1>
 
-<hr style="border:0px;border-bottom: 5px solid #FF9900;margin:5px 0" />
+						<p style="font-size: 14px; margin: 0.5em 0pt 1em; line-height: 1.3em; font-family: Arial; text-align: left;">[CONTENT]</p>
+						</td>
+						<td style="font-size: 14px; line-height: 1.3em; font-family: Arial; text-align: left;"><img alt="" height="20" src="pad.png" style="border: 0px none;" width="20" /></td>
+					</tr>
+				</tbody>
+			</table>
+			<!--content table ends--><!--content table starts-->
 
-
-<table style="background:#EEEEEE;color:#333333;width:100%;margin:10px 0;">
-<tbody><tr><td style="padding:30px 5%;font-size:16px">[CONTENT]</td></tr></tbody>
-</table>
-
-<hr style="border:0px;border-bottom: 5px solid #3366FF;margin:5px 0px" />
-
-<table style="background:#FF9900;width:100%;padding:0;margin:0" cellpadding=0 cellspacing=0>
-<tbody><tr>
-    <td style="color:#000000;font-size:11px;padding:0;margin:0;padding:20px">[FOOTER]</td>
-    <td rowspan="2" style="width:6px;padding:0;margin:0;background:#FFFFFF;border-right:7px solid #FF9900;height:100px"></td>
-    <td rowspan="2" style="width:15px;padding:0;margin:0;background:#FFFFFF;border-right:7px solid #FF9900;height:100px"></td>
-</tr></tbody>
-</table>
-
-<hr style="border:0px;border-bottom: 5px solid #000000;margin:5px 0" />
-
-<table style="width:100%" cellpadding=0 cellspacing=0;>
-<tbody><tr>
-<td>
-    <div style="width:8px;height:8px;background:#3366FF;display:inline-block;margin-right:10px"></div> 
-    <div style="width:8px;height:8px;background:#3366FF;display:inline-block;margin-right:10px"></div> 
-    <div style="width:8px;height:8px;background:#3366FF;display:inline-block;margin-right:10px"></div> 
-</td>
-<td style="text-align:right">
-    <a href="[FORWARDURL]" style="font-size:11px;font-weight:bold;text-decoration:none;color:#FF9900">FORWARD THIS MESSAGE</a>
-</td>
-</tr></tbody>
+			<table border="0" cellpadding="0" cellspacing="0" class="content" style="background-color: rgb(255, 255, 255); color: rgb(102, 102, 102);" width="600">
+				<tbody>
+					<tr valign="top">
+						<td style="font-size: 14px; line-height: 1.3em; font-family: Arial; text-align: left;"><img alt="" height="20" src="pad.png" style="border: 0px none;" width="20" /></td>
+						<td class="maincontent" style="font-size: 14px; margin:5px;font-family: Arial; line-height: 1.3em; text-align: left;">
+						<p style="font-size: 14px; margin: 0.5em 0pt; line-height: 1.3em; font-family: Arial; text-align: left;">[FOOTER]</p>
+						</td>
+						<td style="font-size: 14px; line-height: 1.3em; font-family: Arial; text-align: left;"><img alt="" height="20" src="pad.png" style="border: 0px none;" width="20" /></td>
+					</tr>
+				</tbody>
+			</table>
+			</td>
+		</tr>
+	</tbody>
 </table>
 </div>
+</body>
+</html>
+
 ';
-$template2 = '<p><style type="text/css">#ushuaia h1{font-size:36px}#ushuaia h2{font-size:30px}#ushuaia h3{font-size:24px}#ushuaia h4{font-size:21px}#ushuaia h1,#ushuaia h2,#ushuaia h3,#ushuaia h4,#ushuaia b,#ushuaia a {font-family:\'Times New Roman\', Georgia, serif !Important;color:#000 !important} #ushuaia p{font-family:\'Times New Roman\',Georgia,serif !important;color:#000 !important}</style></p>
-<div style="margin:0; text-align:center; width:98%; background:#EEE;min-width:240px;height:100%;" id="ushuaia">
-<div style="width:98%;margin:0 auto;border:10px dashed #000;">
-<table height="100%" align="center" width="98%">
-    <tbody>
-        <tr>
-            <td height="100px" bgcolor="#EEE" style="border-bottom:1px dotted #888">
-            <h3 style="margin:10px;text-align:center;font-weight:normal; line-height:1; font-size:70px;">[SUBJECT]</h3>
-            </td>
-        </tr>
-        <tr>
-            <td bgcolor="#FFF" style="color:#666;margin:10px">
-            <div style="margin:30px;color:#000 !Important">[CONTENT]</div>
-            </td>
-        </tr>
-        <tr>
-            <td style="background:#CCC;padding:10px;color:#000;text-align:center;font-size:14px;margin:1px 0px;border-top:1px dotted #666">[FOOTER]</td>
-        </tr>
-        <tr>
-            <td style="background:#666;padding:5px;color:#FFF;text-align:center;font-size:11px;margin:1px 0px">[SIGNATURE]</td>
-        </tr>
-    </tbody>
-</table>
-</div>
+$template2 = '<!doctype html>
+<html>
+<head><meta name="viewport" content="width=device-width" /><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>[SUBJECT]</title>
+	<style type="text/css">/* -------------------------------------
+          GLOBAL RESETS
+      ------------------------------------- */
+      
+      /*All the styling goes here*/
+      
+      img {
+        border: none;
+        -ms-interpolation-mode: bicubic;
+        max-width: 100%;
+      }
+    
+      /* -------------------------------------
+          BODY & CONTAINER
+      ------------------------------------- */
+      .body {
+        background-color: #f6f6f6;
+        width: 100%;
+      }
+      /* Set a max-width, and make it display as block so it will automatically stretch to that width, but will also shrink down on a phone or something */
+      .templatecontainer {
+        display: block;
+        margin: 0 auto !important;
+        /* makes it centered */
+        max-width: 580px;
+        padding: 10px;
+        width: 580px;
+      }
+      .logo {
+      padding-bottom: 10px;
+      }
+      
+      /* This should also be a block element, so that it will fill 100% of the .container */
+      .templatecontent {
+        box-sizing: border-box;
+        display: block;
+        margin: 0 auto;
+        max-width: 580px;
+        padding: 10px;
+      }
+      /* -------------------------------------
+          HEADER, FOOTER, MAIN
+      ------------------------------------- */
+      .main {
+        background: #ffffff;
+        border-radius: 3px;
+        width: 100%;
+      }
+      .wrapper {
+        box-sizing: border-box;
+        padding: 20px;
+      }
+      .templatecontent-block {
+        padding-bottom: 10px;
+        padding-top: 10px;
+      }
+      .footer {
+        clear: both;
+        margin-top: 10px;
+        text-align: center;
+        width: 100%;
+      }
+        .footer td,
+        .footer p,
+        .footer span,
+        .footer a {
+          color: #999999;
+          font-size: 12px;
+          text-align: center;
+      }
+      /* -------------------------------------
+          TYPOGRAPHY
+      ------------------------------------- */
+      h1,
+      h2,
+      h3,
+      h4 {
+        color: #000000;
+        font-family: sans-serif;
+        font-weight: 400;
+        line-height: 1.4;
+        margin: 0;
+        margin-bottom: 30px;
+      }
+      h1 {
+        font-size: 35px;
+        font-weight: 300;
+        text-align: center;
+        text-transform: capitalize;
+      }
+      p,
+      ul,
+      ol {
+        font-family: sans-serif;
+        font-size: 14px;
+        font-weight: normal;
+        margin: 0;
+        margin-bottom: 15px;
+      }
+        p li,
+        ul li,
+        ol li {
+          list-style-position: inside;
+          margin-left: 5px;
+      }
+      /* -------------------------------------
+          BUTTONS
+      ------------------------------------- */
+      .templatebtn {
+        box-sizing: border-box;
+        width: 100%; }
+        .templatebtn > tbody > tr > td {
+          padding-bottom: 15px; }
+        .templatebtn table {
+          width: auto;
+      }
+        .templatebtn table td {
+          background-color: #ffffff;
+          border-radius: 5px;
+          text-align: center;
+      }
+        .templatebtn a {
+          background-color: #ffffff;
+          border: solid 1px #3498db;
+          border-radius: 5px;
+          box-sizing: border-box;
+          color: #3498db;
+          cursor: pointer;
+          display: inline-block;
+          font-size: 14px;
+          font-weight: bold;
+          margin: 0;
+          padding: 12px 25px;
+          text-decoration: none;
+          text-transform: capitalize;
+      }
+      .templatebtn-primary table td {
+        background-color: #3498db;
+      }
+      .templatebtn-primary a {
+        background-color: #3498db;
+        border-color: #3498db;
+        color: #ffffff;
+      }
+      /* -------------------------------------
+          OTHER STYLES THAT MIGHT BE USEFUL
+      ------------------------------------- */
+      .last {
+        margin-bottom: 0;
+      }
+      .first {
+        margin-top: 0;
+      }
+      .align-center {
+        text-align: center;
+      }
+      .align-right {
+        text-align: right;
+      }
+      .align-left {
+        text-align: left;
+      }
+      .clear {
+        clear: both;
+      }
+      .mt0 {
+        margin-top: 0;
+      }
+      .mb0 {
+        margin-bottom: 0;
+      }
+      .preheader {
+        color: transparent;
+        display: none;
+        height: 0;
+        max-height: 0;
+        max-width: 0;
+        opacity: 0;
+        overflow: hidden;
+        mso-hide: all;
+        visibility: hidden;
+        width: 0;
+      }
+      .powered-by a {
+        text-decoration: none;
+      }
+      hr {
+        border: 0;
+        border-bottom: 1px solid #f6f6f6;
+        margin: 20px 0;
+      }
+      /* -------------------------------------
+          RESPONSIVE AND MOBILE FRIENDLY STYLES
+      ------------------------------------- */
+      @media only screen and (max-width: 620px) {
+        table[class=body] h1 {
+          font-size: 28px !important;
+          margin-bottom: 10px !important;
+        }
+        table[class=body] p,
+        table[class=body] ul,
+        table[class=body] ol,
+        table[class=body] td,
+        table[class=body] span,
+        table[class=body] a {
+          font-size: 16px !important;
+        }
+        table[class=body] .wrapper,
+        table[class=body] .article {
+          padding: 10px !important;
+        }
+        table[class=body] .templatecontent {
+          padding: 0 !important;
+        }
+        table[class=body] .templatecontainer {
+          padding: 0 !important;
+          width: 100% !important;
+        }
+        table[class=body] .main {
+          border-left-width: 0 !important;
+          border-radius: 0 !important;
+          border-right-width: 0 !important;
+        }
+        table[class=body] .templatebtn table {
+          width: 100% !important;
+        }
+        table[class=body] .templatebtn a {
+          width: 100% !important;
+        }
+        table[class=body] .img-responsive {
+          height: auto !important;
+          max-width: 100% !important;
+          width: auto !important;
+        }
+      }
+      /* -------------------------------------
+          PRESERVE THESE STYLES IN THE HEAD
+      ------------------------------------- */
+      @media all {
+        .ExternalClass {
+          width: 100%;
+        }
+        .ExternalClass,
+        .ExternalClass p,
+        .ExternalClass span,
+        .ExternalClass font,
+        .ExternalClass td,
+        .ExternalClass div {
+          line-height: 100%;
+        }
+        .apple-link a {
+          color: inherit !important;
+          font-family: inherit !important;
+          font-size: inherit !important;
+          font-weight: inherit !important;
+          line-height: inherit !important;
+          text-decoration: none !important;
+        }
+        .templatebtn-primary table td:hover {
+          background-color: #34495e !important;
+        }
+        .templatebtn-primary a:hover {
+          background-color: #34495e !important;
+          border-color: #34495e !important;
+        }
+      }
+	</style>
+</head>
+<body style=" background-color: #f6f6f6;font-family: sans-serif;-webkit-font-smoothing: antialiased;font-size: 14px;line-height: 1.4;margin: 0;padding: 0;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
 <p>&nbsp;</p>
-</div>';
+
+<table border="0" cellpadding="0" cellspacing="0" class="body" role="presentation" style="border-collapse: separate;mso-table-lspace: 0pt;mso-table-rspace: 0pt;width: 100%;">
+	<tbody>
+		<tr>
+			<td style="font-family: sans-serif;font-size: 14px;vertical-align: top;">&nbsp;</td>
+			<td class="container" style="font-family: sans-serif;font-size: 14px;vertical-align: top;">
+			<div class="templatecontent">
+			<div class="logo">
+			<center><img src="[LOGO]" /></center>
+			</div>
+			<!-- START CENTERED WHITE CONTAINER -->
+
+			<table class="main" role="presentation" style="border-collapse: separate;mso-table-lspace: 0pt;mso-table-rspace: 0pt;width: 100%;"><!-- START MAIN CONTENT AREA -->
+				<tbody>
+					<tr>
+						<td class="wrapper" style="font-family: sans-serif;font-size: 14px;vertical-align: top;">
+						<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse: separate;mso-table-lspace: 0pt;mso-table-rspace: 0pt;width: 100%;">
+							<tbody>
+								<tr>
+									<td style="font-family: sans-serif;font-size: 14px;vertical-align: top;"><span class="preheader">This is preheader text. Some clients will show this text as a preview.</span> [CONTENT]</td>
+								</tr>
+							</tbody>
+						</table>
+						</td>
+					</tr>
+					<!-- END MAIN CONTENT AREA -->
+				</tbody>
+			</table>
+			<!-- START FOOTER -->
+
+			<div class="footer">
+			<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse: separate;mso-table-lspace: 0pt;mso-table-rspace: 0pt;width: 100%;">
+				<tbody>
+					<tr>
+						<td class="templatecontent-block" style="font-family: sans-serif;font-size: 14px;vertical-align: top;"><span class="apple-link">[FOOTER]</span>
+
+						<p><a href="[FORWARDURL]" style="color: #3498db; text-decoration: underline;">Forward this message</a></p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			</div>
+			<!-- END FOOTER --><!-- END CENTERED WHITE CONTAINER --></div>
+			</td>
+			<td>&nbsp;</td>
+		</tr>
+	</tbody>
+</table>
+</body>
+</html>
+';
 
 echo formStart();
 echo '<div> 
   <input type="radio" name="template" value="systemtemplate" checked>  System Template<br>
-  <input type="radio" name="template" value="template1"> Template 1<br>
-  <input type="radio" name="template" value="template2"> Template 2<br> 
+  <input type="radio" name="template" value="template1"> Template with Logo<br>
+  <input type="radio" name="template" value="template2"> Simple Responsive Template<br> 
   <input type="submit" value="Submit"  name="Submit">
 </form>
 </div>
@@ -121,11 +417,11 @@ if (isset($_POST['Submit'])) {
             $content = $systemtemplate;
             break;
         case 'template1':
-            $title = "Template one";
+            $title = "Template with Logo";
             $content = $template1;
             break;
         case 'template2':
-            $title = "Template two";
+            $title = "Simple Responsive Template";
             $content = $template2;
             break;
     }
