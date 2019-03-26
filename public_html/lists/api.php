6650a7200b3125ff38f83ab60068a3792d616c0a
Xheni Myrtaj <myrtajxheni@gmail.com>
2019-03-26 23:55:54 +0000

    Add rewrite rule and PHP version check
    Signed-off-by: Xheni Myrtaj <myrtajxheni@gmail.com>

--- public_html/lists/api.php
+++ public_html/lists/api.php
@@ -1,8 +1,10 @@
 <?php
-declare(strict_types=1);
 
 use PhpList\Core\Core\Bootstrap;
 
+if (version_compare(phpversion(), '7.0.0', '<')) {
+  die('API is not supported on this PHP version.');
+}
 require_once __DIR__ . '/base/vendor/autoload.php';
 
 Bootstrap::getInstance()
