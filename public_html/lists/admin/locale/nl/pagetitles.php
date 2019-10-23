31417fe557ad54eeda13da964f59c1adc665aed5
Martijn de Boer <github@sexybiggetje.nl>
2019-10-23 19:19:58 +0200

    Update pagetitles.php
    Correct type, translate untranslated, correct capital usage

--- public_html/lists/admin/locale/nl/pagetitles.php
+++ public_html/lists/admin/locale/nl/pagetitles.php
@@ -1,16 +1,16 @@
 <?php
 
 switch ($page) {
-  case 'home': $page_title = 'Hoofd Admin Pagina'; break;
+  case 'home': $page_title = 'Hoofd admin'; break;
   case 'setup': $page_title = 'Configuratie Opties'; break;
-  case 'about': $page_title = 'About '.NAME; break;
-  case 'attributes': $page_title = 'Configureer Attributen'; break;
+  case 'about': $page_title = 'Over '.NAME; break;
+  case 'attributes': $page_title = 'Configureer attributen'; break;
   case 'stresstest': $page_title = 'Stress Test'; break;
-  case 'list': $page_title = 'De Lijst van de Lijsten'; break;
-  case 'editattributes': $page_title = 'Configureer Attributen'; break;
+  case 'list': $page_title = 'De Lijst der Lijsten'; break;
+  case 'editattributes': $page_title = 'Configureer attributen'; break;
   case 'editlist': $page_title = 'Bewerk een lijst'; break;
-  case 'checki18n': $page_title = 'Controleer of vertaling bestaat'; break;
-  case 'import4': $page_title = 'Importeer emails van een extere database'; break;
+  case 'checki18n': $page_title = 'Controleer of de vertaling bestaat'; break;
+  case 'import4': $page_title = 'Importeer emails van een externe database'; break;
   case 'import3': $page_title = 'Importeer emails van IMAP'; break;
   case 'import2':
   case 'import1':
@@ -18,10 +18,10 @@ switch ($page) {
   case 'export': $page_title = 'Exporteer gebruikers'; break;
   case 'initialise': $page_title = 'Initialiseer de database'; break;
   case 'send': $page_title = 'Verzend een Bericht'; break;
-  case 'preparesend': $page_title = 'Bereidt een bericht voor om te verzenden'; break;
+  case 'preparesend': $page_title = 'Bereid een bericht voor om te verzenden'; break;
   case 'sendprepared': $page_title = 'Verzend een voorbereid bericht'; break;
-  case 'members': $page_title = 'Lijst van Leden'; break;
-  case 'users': $page_title = 'Lijst van alle Gebruikers'; break;
+  case 'members': $page_title = 'Lijst van leden'; break;
+  case 'users': $page_title = 'Lijst van alle gebruikers'; break;
   case 'reconcileusers': $page_title = 'Herwerk gebruikers'; break;
   case 'user': $page_title = 'Details van een gebruiker'; break;
   case 'userhistory': $page_title = 'Geschiedenis van een gebruiker'; break;
@@ -32,11 +32,11 @@ switch ($page) {
   case 'upgrade': $page_title = 'Upgrade '.NAME; break;
   case 'templates': $page_title = 'Templates in het systeem'; break;
   case 'template': $page_title = 'Bewerk of voeg een nieuw sjabloon toe'; break;
-  case 'viewtemplate': $page_title = 'Sjabloon Voorbeeld'; break;
+  case 'viewtemplate': $page_title = 'Sjabloon voorbeeld'; break;
   case 'configure': $page_title = 'Configureeer '.NAME; break;
-  case 'admin': $page_title = 'Bewerk een Administrator'; break;
-  case 'admins': $page_title = 'Lijst van Administrators'; break;
-  case 'adminattributes': $page_title = 'Configureer Administrator attributen'; break;
+  case 'admin': $page_title = 'Bewerk een administrator'; break;
+  case 'admins': $page_title = 'Lijst van administrators'; break;
+  case 'adminattributes': $page_title = 'Configureer administrator attributen'; break;
   case 'processbounces': $page_title = 'zoek bounces van de server op'; break;
   case 'bounces': $page_title = 'Lijst van bounces'; break;
   case 'bounce': $page_title = 'Bekijk een bounce'; break;
@@ -44,12 +44,12 @@ switch ($page) {
   case 'spage': $page_title = 'Inschrijf pagina&acute;s'; break;
   case 'eventlog': $page_title = 'Logboek'; break;
   case 'getrss': $page_title = 'Zoek RSS feeds op'; break;
-  case 'viewrss': $page_title = 'Bekijk RSS Items'; break;
+  case 'viewrss': $page_title = 'Bekijk RSS items'; break;
   case 'community': $page_title = 'Welkom bij de PHPlist community'; break;
   case 'vote': $page_title = 'Stem voor PHPlist'; break;
   case 'login': $page_title = 'Login'; break;
-  case 'logout': $page_title = 'Log Uit'; break;
-  case 'mclicks': $page_title = 'Bericht Klik Statistieken'; break;
-  case 'uclicks': $page_title = 'URL Klik Statistieken'; break;
-  case 'massunconfirm': $page_title = 'Massa Onbevestigde emails'; break;
+  case 'logout': $page_title = 'Log uit'; break;
+  case 'mclicks': $page_title = 'Bericht klik statistieken'; break;
+  case 'uclicks': $page_title = 'URL klik statistieken'; break;
+  case 'massunconfirm': $page_title = 'Massa onbevestigde emails'; break;
 }
