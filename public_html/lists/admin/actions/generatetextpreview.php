2d23b644b46f922ed897edc531e54dae375ce943
Sam Tuke <mail@samtuke.com>
2020-05-14 19:22:10 +0200

    Shorten preview text after other processing to avoid breaking html tags

--- public_html/lists/admin/actions/generatetextpreview.php
+++ public_html/lists/admin/actions/generatetextpreview.php
@@ -11,35 +11,36 @@ verifyCsrfGetToken();
 $msgid = sprintf('%d', $_GET['id']);
 $messagedata = loadMessageData($msgid);
 // shorten to 90 chars (max message preview length)
-$shortMessageData = substr( $messagedata['message'], 0, 90);
 
 // Check if the campaign content is a 'send as a web page' url, and if so, convert that to text instead
-if (preg_match('/\[URL:(.+)\]/', $shortMessageData, $regs)) {
+if (preg_match('/\[URL:(.+)\]/', $messagedata['message'], $regs)) {
     $content = fetchUrl($regs[1]);
     $previewText = HTML2Text($content);
 } else {
-    $previewText = HTML2Text($shortMessageData);
+    $previewText = HTML2Text($messagedata['message']);
 }
+
+$shortPreviewText = substr( $previewText, 0, 90);
 
 // convert to visual preview
 // FIXME this fails when the text is large, or contains £
 
 // replace newlines with spaces
-$previewText = str_replace("\r", "", $previewText);
-$previewText = str_replace("\n", "", $previewText);
+$shortPreviewText = str_replace("\r", "", $shortPreviewText);
+$shortPreviewText = str_replace("\n", "", $shortPreviewText);
 // replace escaped newlines
-$previewText = trim($previewText);
-$previewText = preg_replace("/\n/", '\\n', $previewText);
-$previewText = preg_replace("/\r/", '', $previewText);
+$shortPreviewText = trim($shortPreviewText);
+$shortPreviewText = preg_replace("/\n/", '\\n', $shortPreviewText);
+$shortPreviewText = preg_replace("/\r/", '', $shortPreviewText);
 
 // fix entities
-$previewText = htmlentities($previewText, ENT_IGNORE, 'UTF-8', true);
+$shortPreviewText = htmlentities($shortPreviewText, ENT_IGNORE, 'UTF-8', true);
 
 // replace quotes
-$previewText = str_replace('"', '&quot;', $previewText);
+$shortPreviewText = str_replace('"', '&quot;', $shortPreviewText);
 
 $status = 
 '<script type="text/javascript">
-    $("#messagepreview").val("' .$previewText.'");
+    $("#messagepreview").val("' .$shortPreviewText.'");
 </script>
 ';
