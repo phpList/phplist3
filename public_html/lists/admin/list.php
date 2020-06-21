59112be9d1fb0d7af62cf78241767f197b8a4a67
Xheni Myrtaj <myrtajxheni@gmail.com>
2020-06-21 17:52:36 +0100

    fix bug on the lists page

--- public_html/lists/admin/list.php
+++ public_html/lists/admin/list.php
@@ -218,14 +218,12 @@ if ($total > 30 && empty($_SESSION['showalllists'])) {
 $result = Sql_query('select * from '.$tables['list'].' '.$subselect.$searchLists.' order by listorder '.$limit);
 $numlists = Sql_Affected_Rows($result);
 
-echo formStart(' id="messagefilterform" ');
-
 $searchValue = $_SESSION['searchlists'];
 
 echo '<div> <input type="text" name="search" placeholder="&#128269;'.s('Search lists').'" value="'.htmlentities($searchValue).'" />';
 echo '<button type="submit" name="go" id="filterbutton" >'.s('Go').'</button>
       <button type="submit" name="clear" id="filterclearbutton" value="1">'.s('Clear').'</button>';
-echo '</div> </form>';
+echo '</div> ';
 $ls = new WebblerListing($total.' '.s('Lists'));
 $ls->usePanel($paging);
 
