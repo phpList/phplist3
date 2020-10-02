ca2c934db2dff6d78493f0a757b6e2345f807d33
Michiel Dethmers <git@phplist.com>
2020-10-02 15:33:15 +0100

    only set session params if the session is not already active

--- public_html/lists/admin/init.php
+++ public_html/lists/admin/init.php
@@ -46,10 +46,12 @@ if (!defined('DEVVERSION')) {
     define('DEVVERSION', false);
 }
 
-ini_set('session.name','phpListSession');
-ini_set('session.cookie_samesite','Strict');
-ini_set('session.use_only_cookies',1);
-ini_set('session.cookie_httponly',1);
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    ini_set('session.name','phpListSession');
+    ini_set('session.cookie_samesite','Strict');
+    ini_set('session.use_only_cookies',1);
+    ini_set('session.cookie_httponly',1);
+}
 
 // record the start time(usec) of script
 $now = gettimeofday();
