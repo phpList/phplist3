654fa0a536d480e709d96dd8eba358559231d551
nepomuc <info@nepomuc.com>
2020-10-28 08:13:02 +0100

    Added support for multiple ACCESS_CONTROL_ALLOW_ORIGINS in order to make ajax based signup forms from multiple domains but without using * for access-control-allow-origin header.

--- public_html/lists/admin/init.php
+++ public_html/lists/admin/init.php
@@ -653,7 +653,9 @@ if (!defined('PHPLIST_POWEREDBY_URLROOT')) {
 if (!isset($allowed_referrers) || !is_array($allowed_referrers)) {
     $allowed_referrers = array();
 }
-if (!defined('ACCESS_CONTROL_ALLOW_ORIGIN')) {
+if (defined('ACCESS_CONTROL_ALLOW_ORIGINS') && in_array($_SERVER['HTTP_ORIGIN'], ACCESS_CONTROL_ALLOW_ORIGINS)) {
+    define('ACCESS_CONTROL_ALLOW_ORIGIN', $_SERVER['HTTP_ORIGIN']);
+} elseif (!defined('ACCESS_CONTROL_ALLOW_ORIGIN')) {
     define('ACCESS_CONTROL_ALLOW_ORIGIN', $GLOBALS['scheme'].'://'.$_SERVER['HTTP_HOST']);
 }
 
