ec3e35aa673bdee54a41b317ec0e0fb108cca588
Michiel Dethmers <git@phplist.com>
2021-01-11 21:19:55 +0000

    avoid fetching the RSS feed in PHP8 as it breaks things

--- public_html/lists/admin/communityfeed.php
+++ public_html/lists/admin/communityfeed.php
@@ -51,6 +51,8 @@ function buildNews($rss, $max)
  * in the session.
  */
 $newsSize = 'long';
+## OnyxRSS doesn't work in PHP8 
+if (PHP_VERSION_ID >= 80000) return;
 
 if (empty($_SESSION['adminloggedin'])
     || (isset($_GET['page']) && !in_array($_GET['page'], array('home', 'about', 'dashboard', 'community', 'login')))) {
