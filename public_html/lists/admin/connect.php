b10c45b5fb0d7c53b053cd4212ba8085edb3647c
Duncan Cameron <phplist@dcameron.me.uk>
2021-10-24 11:11:01 +0100

    Avoid php 8 fatal error when trying to format an empty date string

--- public_html/lists/admin/connect.php
+++ public_html/lists/admin/connect.php
@@ -1794,6 +1794,9 @@ function monthName($month, $short = 0)
  */
 function formatDate($date, $short = 0)
 {
+    if ($date == '') {
+        return '';
+    }
     $format = getConfig('date_format');
     $year = substr($date, 0, 4);
     $month = substr($date, 5, 2);
