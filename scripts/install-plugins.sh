bb2a9b699d6da194fdbcb2678a992d1cfbfdc6b3
Michiel Dethmers <michiel@phplist.com>
2021-12-04 21:04:25 +0000

    install rsync if necessary

--- scripts/install-plugins.sh
+++ scripts/install-plugins.sh
@@ -7,6 +7,10 @@ to=$2
 
 echo $from $to
 
+[[ -x $(which rsync) ]] || {
+  apt install -y rsync
+}
+
 for plugin in $(find $from -type d -name phplist-plugin-*); do
   [[ ! -z "$(ls -A $plugin/plugins/)" ]] && rsync -a $plugin/plugins/* $to
 done
