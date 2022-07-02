043727d5cd463afec1d71bb90fcb55eb84b14659
Duncan Cameron <phplist@dcameron.me.uk>
2022-07-02 11:05:19 +0100

    Split the part of shortening text that needs to be multibyte aware into a separate function.

--- public_html/lists/admin/lib.php
+++ public_html/lists/admin/lib.php
@@ -1924,55 +1924,64 @@ function listCategories()
 }
 
 /*
+ * shortenText
+ *
+ * Shorten text for use by shortenTextDisplay() but also stand-alone.
+ *
+ * Define multibyte-string aware/unaware function depending on whether the mbstring extension is available
+ * see https://github.com/phpList/phplist3/pull/10
+ */
+if (!function_exists('mb_strlen')) {
+    // mbstring unavailable
+    function shortenText($text, $max = 30)
+    {
+        if (strlen($text) > $max) {
+            if ($max < 30) {
+                $shortened = substr($text, 0, $max - 4).' ... ';
+            } else {
+                $shortened = substr($text, 0, 20).' ... '.substr($text, -10);
+            }
+        } else {
+            $shortened = $text;
+        }
+
+        return $shortened;
+    }
+} else {
+    // mbstring available
+    function shortenText($text, $max = 30)
+    {
+        if (mb_strlen($text) > $max) {
+            if ($max < 30) {
+                $shortened = mb_substr($text, 0, $max - 4).' ... ';
+            } else {
+                $shortened = mb_substr($text, 0, 20).' ... '.mb_substr($text, -10);
+            }
+        } else {
+            $shortened = $text;
+        }
+
+        return $shortened;
+    }
+}
+
+/*
  * shortenTextDisplay
  *
  * mostly used for columns in listings to retrict the width, particularly on mobile devices
  * it will show the full text as the title tip but restrict the size of the output
  *
  * will also place a space after / and @ to facilitate wrapping in the browser
+ *
  */
-
 function shortenTextDisplay($text, $max = 30)
 {
-    //# use mb_ version if possible, see https://github.com/phpList/phplist3/pull/10
-    if (function_exists('mb_strlen')) {
-        return mb_shortenTextDisplay($text, $max);
-    }
-
-    $text = preg_replace('!^https?://!i', '', $text);
-    if (strlen($text) > $max) {
-        if ($max < 30) {
-            $display = substr($text, 0, $max - 4).' ... ';
-        } else {
-            $display = substr($text, 0, 20).' ... '.substr($text, -10);
-        }
-    } else {
-        $display = $text;
-    }
+    $display = preg_replace('!^https?://!i', '', $text);
+    $display = shortenText($display, $max);
     $display = str_replace('/', '/&#x200b;', $display);
     $display = str_replace('@', '@&#x200b;', $display);
 
-    return sprintf('<span title="%s">%s</span>', htmlspecialchars($text),
-        htmlspecialchars($text), $display);
-}
-
-function mb_shortenTextDisplay($text, $max = 30)
-{
-    $text = preg_replace('!^https?://!i', '', $text);
-    if (mb_strlen($text) > $max) {
-        if ($max < 30) {
-            $display = mb_substr($text, 0, $max - 4).' ... ';
-        } else {
-            $display = mb_substr($text, 0, 20).' ... '.mb_substr($text, -10);
-        }
-    } else {
-        $display = $text;
-    }
-    $display = str_replace('/', '/&#x200b;', $display);
-    $display = str_replace('@', '@&#x200b;', $display);
-
-    return sprintf('<span title="%s" ondblclick="alert(\'%s\');">%s</span>', htmlspecialchars($text),
-        htmlspecialchars($text), $display);
+    return sprintf('<span title="%s">%s</span>', htmlspecialchars($text), $display);
 }
 
 if (!function_exists('getnicebacktrace')) {
