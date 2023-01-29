fabf6cb208926f996f4ed10c122cb9dd1491918d
Duncan Cameron <phplist@dcameron.me.uk>
2023-01-29 13:08:10 +0000

    Avoid php warning when template does not exist

--- public_html/lists/admin/sendemaillib.php
+++ public_html/lists/admin/sendemaillib.php
@@ -1443,16 +1443,18 @@ function precacheMessage($messageid, $forwardContent = 0)
 
     $cached[$messageid]['htmlformatted'] = strip_tags($cached[$messageid]['content']) != $cached[$messageid]['content'];
     $cached[$messageid]['sendformat'] = $message['sendformat'];
+
+    $cached[$messageid]['template'] = '';
+    $cached[$messageid]['template_text'] = '';
+    $cached[$messageid]['templateid'] = 0;
     if ($message['template']) {
         $req = Sql_Fetch_Row_Query("select template, template_text from {$GLOBALS['tables']['template']} where id = {$message['template']}");
-        $cached[$messageid]['template'] = stripslashes($req[0]);
-        $cached[$messageid]['template_text'] = stripslashes($req[1]);
-        $cached[$messageid]['templateid'] = $message['template'];
-        //   dbg("TEMPLATE: ".$req[0]);
-    } else {
-        $cached[$messageid]['template'] = '';
-        $cached[$messageid]['template_text'] = '';
-        $cached[$messageid]['templateid'] = 0;
+        if ($req) {
+            $cached[$messageid]['template'] = stripslashes($req[0]);
+            $cached[$messageid]['template_text'] = stripslashes($req[1]);
+            $cached[$messageid]['templateid'] = $message['template'];
+            //   dbg("TEMPLATE: ".$req[0]);
+        }
     }
 
     //# @@ put this here, so it can become editable per email sent out at a later stage
