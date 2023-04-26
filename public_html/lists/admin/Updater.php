7a88728a1871c95d74c2ba617b9195ed4f0c05c6
Duncan Cameron <3147688+bramley@users.noreply.github.com>
2023-04-26 16:53:51 +0100

    Allow a plugin to update phplist (#946)
    * Allow a plugin to update the phplist code
    
    * When a new release is available link to a plugin if available in preference to the updater

---
+++ public_html/lists/admin/Updater.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * A plugin should implement this interface if it wants to be responsible for updating phpList.
+ * It should implement an admin page with the name "update".
+ */
+interface Updater
+{
+}
