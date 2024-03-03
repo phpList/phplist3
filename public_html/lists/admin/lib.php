ecc9a56f51063805c7c6fd9cb991a239c9b2843f
Michiel Dethmers <michiel@phplist.com>
2024-03-03 13:14:39 +0000

    check on valid var and cast to int

--- public_html/lists/admin/lib.php
+++ public_html/lists/admin/lib.php
@@ -942,7 +942,7 @@ function getPageLock($force = 0)
     // while ($running_res['age'] && $count >= $max) { # a process is already running
     while ($count >= $max) { // don't check age, as it may be 0
         //   cl_output('running process: '.$running_res['age'].' '.$max);
-        if ($running_res['age'] > 600) {
+        if (!empty($running_res['age']) && (int)$running_res['age'] > 600) {
             // some sql queries can take quite a while
             //cl_output($running_res['id'].' is old '.$running_res['age']);
             // process has been inactive for too long, kill it
