53f8c9bd5444a31f6ded1d3bfbb915d050023374
Tatevik <tatevikg1@gmail.com>
2024-11-24 17:33:08 +0400

    ISSUE-337: use api php8.1 version

--- public_html/lists/api.php
+++ public_html/lists/api.php
@@ -2,7 +2,7 @@
 
 use PhpList\Core\Core\Bootstrap;
 
-if (version_compare(phpversion(), '7.0.0', '<')) {
+if (version_compare(phpversion(), '8.1', '<')) {
   die('API is not supported on this PHP version.');
 }
 require_once __DIR__ . '/base/vendor/autoload.php';
