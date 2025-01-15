08492b743ecb61d18e87650e6389f6fff7e5464c
Michiel Dethmers <michiel@phplist.com>
2025-01-15 17:09:24 +0000

    Change env vars for docker (#1060)
    * update PR template
    
    * rename the environment variables for docker, so they match the API

--- docker/docker-phplist-config-live.php
+++ docker/docker-phplist-config-live.php
@@ -1,10 +1,33 @@
 <?php
 
-$database_host = getenv('DB_HOST');
-$database_name = getenv('DB_NAME');
-$database_user = getenv('DB_USER');
-$database_password = getenv('DB_PASSWORD');
+$database_host_legacy = getenv('DB_HOST');
+$database_name_legacy = getenv('DB_NAME');
+$database_user_legacy = getenv('DB_USER');
+$database_password_legacy = getenv('DB_PASSWORD');
+
+$database_host = getenv('PHPLIST_DATABASE_HOST');
+$database_port = getenv('PHPLIST_DATABASE_PORT');
+$database_driver = getenv('PHPLIST_DATABASE_DRIVER'); ## only pdo_mysql at the moment
+$database_name = getenv('PHPLIST_DATABASE_NAME');
+$database_user = getenv('PHPLIST_DATABASE_USER');
+$database_password = getenv('PHPLIST_DATABASE_PASSWORD');
+$phplist_secret = getenv('PHPLIST_SECRET');
+
+if (empty($database_host)) {
+    $database_host = $database_host_legacy;
+}
+if (empty($database_name)) {
+    $database_name = $database_name_legacy;
+}
+if (empty($database_user)) {
+    $database_user = $database_user_legacy;
+}
+if (empty($database_password)) {
+    $database_password = $database_password_legacy;
+}
+
 $mailhost = getenv('MAILHOST');
+
 define('PHPMAILERHOST', $mailhost);
 define('TEST', 0);
 define('HASH_ALGO', 'sha256');
