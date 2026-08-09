5159c3ed33a5674a8169472b50d146d97f630746
Tatevik <tatevik@phplist.com>
2026-08-09 15:00:25 +0400

    verifyCsrfGetToken on delete actions (#1121)
    Co-authored-by: Tatevik <tatevikg1@gmail.com>

--- public_html/lists/admin/admins.php
+++ public_html/lists/admin/admins.php
@@ -40,6 +40,7 @@ echo '<div class="button">'.PageLink2('importadmin', s('Import list of admins'))
 echo '<div class="pull-right fright">'.PageLinkActionButton('admin', s('Add new admin'), "start=$start".$remember_find).'</div><div class="clearfix"></div>';
 
 if (isset($_GET['delete']) && $_GET['delete']) {
+    verifyCsrfGetToken();
     // delete the index in delete
     if ($_GET['delete'] == $_SESSION['logindetails']['id']) {
         echo s('You cannot delete yourself')."\n";
